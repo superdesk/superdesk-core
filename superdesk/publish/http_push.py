3743fc73665ab92909ca4edb6a155ebf61242e3e
mugur.rus <mugur.rus@sourcefabric.org>
2015-09-11 19:00:51 +0300

    HTTP Push publish service improvements

--- superdesk/publish/http_push.py
+++ superdesk/publish/http_push.py
@@ -10,25 +10,16 @@
 
 import json
 
-from superdesk import config, app
+from eve.utils import config
+import requests
+
+from superdesk import app
 from superdesk.errors import PublishHTTPPushError
 from superdesk.publish import register_transmitter
 from superdesk.publish.publish_service import PublishService
-from io import BytesIO
-from bson import ObjectId
-from urllib.parse import urlparse
-from http.client import HTTPConnection, HTTPSConnection, InvalidURL
-from copy import copy
 
 
 errors = [PublishHTTPPushError.httpPushError().get_error_description()]
-
-
-class ItemNotFound(Exception):
-    pass
-
-class IdConflict(Exception):
-    pass
 
 
 class HTTPPushService(PublishService):
@@ -36,121 +27,55 @@ class HTTPPushService(PublishService):
     headers = {"Content-type": "application/json", "Accept": "application/json"}
 
     def _transmit(self, queue_item, subscriber):
-        config = queue_item.get('destination', {}).get('config', {})
+        """
+        @see: PublishService._transmit
+        """
+        item = json.loads(queue_item['formatted_item'])
+        item['guid'] = item[config.ID_FIELD]
+        del item[config.ID_FIELD]
 
+        assets_url = queue_item.get('destination', {}).get('config', {}).get('assets_url')
         try:
-            self._copy_published_media_files(json.loads(queue_item['formatted_item']))
+            self._copy_published_media_files(json.loads(queue_item['formatted_item']), assets_url)
+        except Exception as e:
+            raise PublishHTTPPushError.httpPushError(e, queue_item.get('destination', {}))
 
-            try:
-                self._post(config.get('resource_url'), queue_item['formatted_item'])
-            except IdConflict:
-                response = self._get(config.get('item_url'), queue_item['item_id'])
-                read_item = json.loads(response.read().decode('utf_8'))
-                self._patch(config.get('item_url'), queue_item['item_id'], queue_item['formatted_item'], read_item['_etag'])
-        except Exception as ex:
-            raise PublishHTTPPushError.httpPushError(ex, queue_item.get('destination', {}))
+        resource_url = queue_item.get('destination', {}).get('config', {}).get('resource_url')
+        response = requests.post(resource_url, json=item)
+        if response.status_code != requests.codes.created:  # @UndefinedVariable
+            raise PublishHTTPPushError.httpPushError(Exception('Error pushing item %s' % response.text),
+                                                     queue_item.get('destination', {}))
 
-    def _get(self, url, item_id=None):
-        '''Retrieve an item from the given resource url
-        Raises ItemNotFound if the item identified by item_id (if given) did not exist
-        Raises Exception on other errors
-
-        @param url: the resource url
-        @param item_id: the item identifier
-        @return: HTTPResponse object
-        '''
-        if item_id:
-            url = url.format(item_id)
-        request_client = self._request_client(url)
-        request_client.request('GET', url)
-        response = request_client.getresponse()
-        if response.status == 404:
-            raise ItemNotFound()
-        if response.status != 200:
-            raise Exception('Error retrieving url %s: %s' % (url, response.reason))
-        return response
-
-    def _post(self, url, formatted_item):
-        '''Send a post request to content API
-        Raises IdConflict if the item identifier already existed
-        Raises Exception on other errors
-
-        @param url: the resource url
-        @param formatted_item: string containing the item in the required format
-        @return: HTTPResponse object
-        '''
-        request_client = self._request_client(url)
-        request_client.request('POST', url, formatted_item, self.headers)
-        response = request_client.getresponse()
-        if response.status == 409:
-            raise IdConflict()
-        if response.status != 201:
-            raise Exception('Error pushing item %s' % response.reason)
-        return response
-
-    def _headers(self, *custom_headers):
-        '''Return headers for an HTTP request
-
-        @param custom_headers: tuples of header-value pairs
-        '''
-        updated_headers = copy(self.headers)
-        updated_headers.update(custom_headers)
-        return updated_headers
-
-    def _patch(self, item_url, _id, formatted_item, etag):
-        '''Send a patch request to content API to update a given item
-        Raises ItemNotFound if the item identified by _id did not exist
-        Raises Exception on other errors
-
-        @param item_url: the format of the item url
-        @param _id: the item identifier
-        @param formatted_item: string containing the item in the required format
-        @param etag: the etag of the existing item
-        @return: HTTPResponse object
-        '''
-        url = item_url.format(_id)
-        headers = self._headers(self.headers, ('If-Match', etag))
-        request_client = self._request_client(url)
-        request_client.request('PATCH', url, formatted_item, headers)
-        response = request_client.getresponse()
-        if response.status == 404:
-            raise ItemNotFound()
-        if response.status < 200 or response.status >= 300:
-            raise Exception('Error pushing item %s' % response.reason)
-        return response
-
-    def _request_client(self, url):
-        '''Return a HTTP request object that corresponds to the given url
-        Raises PublishHTTPPushError on invalid url
-
-        @param url: The URL for which to return the HTTP request client object
-        '''
-        url_parts = urlparse(url)
-        if url_parts.scheme.lower() == 'http':
-            return HTTPConnection(url_parts.netloc)
-        elif url_parts.scheme.lower() == 'https':
-            return HTTPSConnection(url_parts.netloc)
-        else:
-            raise PublishHTTPPushError.httpPushError(InvalidURL('Invalid HTTP URL %s' % url), config)
-
-    def _copy_published_media_files(self, item):
-        '''Copy the media files for the given item to the publish_items endpoint
+    def _copy_published_media_files(self, item, assets_url):
+        """Copy the media files for the given item to the publish_items endpoint
 
         @param item: the item object
-        '''
-        for k, v in item.get('renditions', {}).items():
-            if k == 'original_source':
-                # if the source is AAP Multimedia then don't copy
-                continue
+        @type item: dict
+        @param assets_url: the url where the media can be uploaded
+        @type assets_url: string
+        """
+        for name, rendition in item.get('renditions', {}).items():
+            del item['renditions'][name]['href']
+            if not self._media_exists(rendition['media'], assets_url):
+                media = app.media.get(rendition['media'], resource='upload')
+                files = {'media': (rendition['media'], media, rendition.get('mimetype') or rendition['mime_type'])}
+                response = requests.post(assets_url, files=files, data={'media_id': rendition['media']})
+                if response.status_code != requests.codes.created:  # @UndefinedVariable
+                    raise Exception('Error pushing item %s media file %s' % (item._id, rendition['media']))
 
-            del v['href']
-            if not app.media.exists(v['media'], resource='publish_items'):
-                img = app.media.get(v['media'], resource='upload')
-                content = BytesIO(img.read())
-                content.seek(0)
-                _id = app.media.put(content, filename=img.filename, content_type=img.content_type,
-                                    metadata=img.metadata, resource='publish_items', _id=ObjectId(v['media']))
-                assert str(_id) == v['media']
+    def _media_exists(self, media_id, assets_url):
+        """Returns true if the media with the given id exists at the service identified by assets_url.
+        Returns false otherwise. Raises Exception if the error code was not 200 or 404
 
+        @param media_id: the media identifier
+        @type media_id: string
+        @param assets_url: the url of the assest service
+        @type assets_url: string
+        @return: bool
+        """
+        response = requests.get('%s/%s' % (assets_url, media_id))
+        if response.status_code not in (requests.codes.ok, requests.codes.not_found):  # @UndefinedVariable
+            raise Exception('Error querying the assets service %s' % assets_url)
+        return response.status_code == requests.codes.ok  # @UndefinedVariable
 
-register_transmitter('HTTPPush', HTTPPushService(), errors)
+register_transmitter('http_push', HTTPPushService(), errors)
--- superdesk/publish/http_push_tests.py
+++ superdesk/publish/http_push_tests.py
@@ -12,10 +12,14 @@
 import json
 import os
 import unittest
+import requests
 
 from superdesk.publish import SUBSCRIBER_TYPES
-from superdesk.publish.http_push import HTTPPushService, ItemNotFound
-from time import sleep
+from superdesk.publish.http_push import HTTPPushService
+
+
+class ItemNotFound(Exception):
+    pass
 
 
 class HTTPPushPublishTestCase(unittest.TestCase):
@@ -23,32 +27,53 @@ class HTTPPushPublishTestCase(unittest.TestCase):
     def setUp(self):
         super().setUp()
 
-        if 'HTTP_PUSH_RESOURCE_URL' not in os.environ or 'HTTP_PUSH_ITEM_URL' not in os.environ:
+        if 'HTTP_PUSH_RESOURCE_URL' not in os.environ:
             return
         self.resource_url = os.environ['HTTP_PUSH_RESOURCE_URL']
-        self.item_url = os.environ['HTTP_PUSH_ITEM_URL']
 
         self.subscribers = [{"_id": "1", "name": "Test", "media_type": "media",
                              "subscriber_type": SUBSCRIBER_TYPES.DIGITAL, "is_active": True,
                              "sequence_num_settings": {"max": 10, "min": 1},
                              "destinations": [{"name": "test", "delivery_type": "http_push", "format": "ninjs",
-                                               "config": {"resource_url": self.resource_url, "item_url": self.item_url}
+                                               "config": {"resource_url": self.resource_url}
                                                }]}]
+        self.formatted_item1 = {"_id": "item1",
+                                "headline": "headline",
+                                "versioncreated": "2015-03-09T16:32:23",
+                                "version": 1
+                                }
+        self.formatted_item2 = {"_id": "item1",
+                                "headline": "headline2",
+                                "versioncreated": "2015-03-21T13:43:51",
+                                "version": 2
+                                }
         self.item = {'item_id': 'item1',
-                     'format': 'NITF',
+                     'format': 'ninjs',
                      'item_version': 1,
                      'published_seq_num': 1,
-                     'formatted_item': '{"_id": "item1", "headline": "headline"}',
+                     'formatted_item': json.dumps(self.formatted_item1),
                      'destination': {"name": "test", "delivery_type": "http_push", "format": "ninjs",
-                                     "config": {"resource_url": self.resource_url, "item_url": self.item_url}}
-                     }
+                                     "config": {"resource_url": self.resource_url}
+                                     }}
 
-    def is_item_published(self, url, item_id):
-        try:
-            HTTPPushService()._get(url, item_id)
-        except ItemNotFound:
+    def is_item_published(self, item_id):
+        """Return True if the item was published, False otherwise.
+        Raises Exception in case of server/communication error.
+        """
+        response = requests.get(self.getItemURL(item_id))
+        if response.status_code == requests.codes.not_found:  # @UndefinedVariable
             return False
+        self.assertEqual(response.status_code, requests.codes.ok,  # @UndefinedVariable
+                         'Error retrieving item from the content API')
         return True
+
+    def getItemURL(self, item_id):
+        """Returns the URL for item read
+
+        @param item_id: the item identifier
+        @return: string
+        """
+        return '%s/%s' % (self.resource_url, item_id)
 
     def test_publish_an_item(self):
         if not getattr(self, 'resource_url', None):
@@ -57,11 +82,10 @@ class HTTPPushPublishTestCase(unittest.TestCase):
         service = HTTPPushService()
 
         service._transmit(self.item, self.subscribers)
-        self.assertTrue(self.is_item_published(self.item_url, self.item['item_id']))
-        sleep(0.5)
+        self.assertTrue(self.is_item_published(self.item['item_id']))
 
-        self.item['formatted_item'] = '{"_id": "item1", "headline": "headline2"}'
+        self.item['formatted_item'] = json.dumps(self.formatted_item2)
         service._transmit(self.item, self.subscribers)
-        response = HTTPPushService()._get(self.item_url, self.item['item_id'])
-        item = json.loads(response.read().decode(encoding='utf_8'))
+        item = requests.get(self.getItemURL(self.item['item_id'])).json()
         self.assertEqual(item['headline'], 'headline2')
+        self.assertEqual(item['version'], 2)
