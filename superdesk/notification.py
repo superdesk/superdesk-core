7887e3d9ee2eb370ef999053d89dc1a8781d33f4
petr.jasek <petr.jasek@sourcefabric.org>
2015-10-21 19:26:58 +0200

    refactor(notifications): refactor using websockets library
    so this should avoid memory leak, also now it knows when there
    is server (pub) or client (sub) connection and only send messages
    from servers to clients.
    
    when server can't connect to websocket it will try next time it
    will be sending a message.
    
    SD-3385 SD-3480

--- superdesk/notification.py
+++ superdesk/notification.py
@@ -10,59 +10,77 @@
 
 """Superdesk push notifications"""
 
+import os
 import logging
 import asyncio
+import websockets
 
-from flask import current_app as app
 from flask import json
+from flask import current_app as app
 from datetime import datetime
 
 
+logging.basicConfig()
 logger = logging.getLogger(__name__)
 
 
-def init_app(app):
-    from autobahn.asyncio.websocket import WebSocketClientProtocol
-    from autobahn.asyncio.websocket import WebSocketClientFactory
-    host = app.config['WS_HOST']
-    port = app.config['WS_PORT']
+class ClosedSocket():
+    """Mimic closed socket to simplify logic when connection
+    can't be estabilished at first place.
+    """
+    def __init__(self):
+        self.open = False
 
-    class Client(WebSocketClientProtocol):
-
-        def __init__(self, *args):
-            WebSocketClientProtocol.__init__(self, *args)
-            self.opened = asyncio.Future()
-
-        def notify(self, **kwargs):
-            kwargs.setdefault('_created', datetime.utcnow().isoformat())
-            self.sendMessage(json.dumps(kwargs).encode('utf8'))
-
-        def onConnect(self, response):
-            self.opened.set_result(None)
-
-    factory = WebSocketClientFactory()
-    factory.protocol = Client
-    try:
-        loop = asyncio.get_event_loop()
-        coro = loop.create_connection(factory, host, port)
-        _transport, client = loop.run_until_complete(coro)
-        loop.run_until_complete(client.opened)
-        app.notification_client = client
-    except OSError:  # no ws server running
+    def close(self):
         pass
 
 
+def init_app(app):
+    """Create websocket connection and put it on app object."""
+    host = app.config['WS_HOST']
+    port = app.config['WS_PORT']
+    loop = asyncio.get_event_loop()
+    try:
+        app.ws_client = loop.run_until_complete(websockets.connect('ws://%s:%s/server' % (host, port)))
+        logger.info('websocket connected on=%s:%s' % app.ws_client.local_address)
+    except OSError as err:
+        # not working now, but we can try later when actually sending something
+        logger.exception(err)
+        app.ws_client = ClosedSocket()
+
+
+def _notify(**kwargs):
+    """Send out all kwargs as json string."""
+    kwargs.setdefault('_created', datetime.utcnow().isoformat())
+    kwargs.setdefault('_process', os.getpid())
+    message = json.dumps(kwargs)
+
+    @asyncio.coroutine
+    def send_message():
+        yield from app.ws_client.send(message)
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(send_message())
+
+
 def push_notification(name, **kwargs):
+    """Push notification to websocket.
+
+    In case socket is closed it will try to reconnect.
+
+    :param name: event name
+    """
     logger.info('pushing event {0} ({1})'.format(name, json.dumps(kwargs)))
 
-    if app.notification_client is None:
-        logger.info('Notification server is not initialized. Try to reinitialize the connection again')
+    if not app.ws_client.open:
+        app.ws_client.close()
         init_app(app)
 
-    if app.notification_client is not None:
-        try:
-            app.notification_client.notify(event=name, extra=kwargs)
-        except AttributeError:
-            logger.info('Notification server is not initialized')
-        except Exception as e:
-            logger.exception(e)
+    if not app.ws_client.open:
+        logger.error('No connection to websocket server. Dropping event %s' % name)
+        return
+
+    try:
+        _notify(event=name, extra=kwargs)
+    except Exception as err:
+        logger.exception(err)
