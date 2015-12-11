b145222b240f964798a480b9e97921b908afe8c2
Siva Kuna <skuna@aap.com.au>
2015-12-11 11:26:23 +1100

    [SD-3810] New (pending) user that has not followed the email link to set password already gets email notifications

--- tests/activity_tests.py
+++ tests/activity_tests.py
@@ -8,11 +8,28 @@
 # AUTHORS and LICENSE files distributed with this source code, or
 # at https://www.sourcefabric.org/superdesk/license
 
-import unittest
-from superdesk.activity import ActivityService
+from unittest import mock
+
+from superdesk.activity import ActivityService, ACTIVITY_ERROR, get_recipients
+from superdesk.publish import init_app
+from superdesk.tests import TestCase
 
 
-class ActivityTestCase(unittest.TestCase):
+def mock_get_resource_service(resource_name):
+    return MockPreferenceService()
+
+
+class MockPreferenceService:
+    def email_notification_is_enabled(self, preferences=None):
+        send_email = preferences.get('email:notification', {}) if isinstance(preferences, dict) else {}
+
+        return send_email and send_email.get('enabled', False)
+
+
+class ActivityTestCase(TestCase):
+    def setUp(self):
+        super().setUp()
+        init_app(self.app)
 
     def test_if_user_is_in_recipients(self):
         activity = {'recipients': [{'user_id': '1', 'read': True},
@@ -29,3 +46,13 @@ class ActivityTestCase(unittest.TestCase):
         self.assertTrue(ActivityService().is_read(activity, '1'))
         self.assertFalse(ActivityService().is_read(activity, '2'))
         self.assertFalse(ActivityService().is_read(activity, '3'))
+
+    @mock.patch('superdesk.activity.get_resource_service', mock_get_resource_service)
+    def test_get_recipients_filters_out_users_not_activated(self):
+        users = [{'email': 'test_1@test.com', 'needs_activation': False, 'is_enabled': True, 'is_active': True,
+                  'user_preferences': {'email:notification': {'enabled': True}}},
+                 {'email': 'test_2@test.com', 'needs_activation': True, 'is_enabled': True, 'is_active': True,
+                  'user_preferences': {'email:notification': {'enabled': True}}}]
+
+        recipients = get_recipients(user_list=users, activity_name=ACTIVITY_ERROR)
+        self.assertEqual(len(recipients), 1)
