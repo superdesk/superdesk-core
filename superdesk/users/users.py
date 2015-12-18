ff4666ae5de7474eb03a8c6606aed0917d8bdd0e
Anca Farcas <anca.farcas@sourcefabric.org>
2015-12-18 13:50:50 +0200

    back-end support for removing user avatar

--- superdesk/users/users.py
+++ superdesk/users/users.py
@@ -65,7 +65,7 @@ class UsersResource(Resource):
                 'type': 'string',
                 'nullable': True
             },
-            'avatar': Resource.rel('upload', True),
+            'avatar': Resource.rel('upload', embeddable=True, nullable=True),
             'role': Resource.rel('roles', True),
             'privileges': {'type': 'dict'},
             'workspace': {
