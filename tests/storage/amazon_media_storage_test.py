c1816c28787a161864bee4ef77c8e90196d39c66
marwoodandrew <amarwood@aap.com.au>
2015-12-31 11:40:13 +1100

    fix(amazon) exception thrown when trying to push to AWS s3

--- tests/storage/amazon_media_storage_test.py
+++ tests/storage/amazon_media_storage_test.py
@@ -3,12 +3,12 @@ import time
 from superdesk.tests import TestCase
 from superdesk.storage.amazon.amazon_media_storage import AmazonMediaStorage
 
-
 class AmazonMediaStorageTestCase(TestCase):
 
     def setUp(self):
         super().setUp()
         self.app.config['AMAZON_CONTAINER_NAME'] = 'AMAZON_CONTAINER_NAME'
+        self.app.config['AMAZON_REGION'] = 'us-east-1'
         self.app.config['AMAZON_S3_USE_HTTPS'] = True
         self.app.config['AMAZON_SERVE_DIRECT_LINKS'] = True
         self.amazon = AmazonMediaStorage(self.app)
@@ -29,3 +29,26 @@ class AmazonMediaStorageTestCase(TestCase):
         self.app.config['AMAZON_REGION'] = 'eu-west-1'
         self.assertEqual(self.amazon.url_for_media(media_id),
                          'http://AMAZON_CONTAINER_NAME.s3-eu-west-1.amazonaws.com/%s' % (media_id))
+
+
+class AmazonMediaStoragePutAndDeleteTest(TestCase):
+    def setUp(self):
+        super().setUp()
+        self.amazon = AmazonMediaStorage(self.app)
+
+    def test_put_and_delete(self):
+        """
+        If the environment variables have a Amazon secret key set then assume that we can attempt to put and delete
+        into s3
+        :return:
+        """
+        if self.app.config['AMAZON_SECRET_ACCESS_KEY']:
+            id = self.amazon.put('test', content_type='text/plain')
+            self.assertIsNot(id,None)
+            self.assertTrue(self.amazon.exists(id))
+            fromS3 = self.amazon.get(id)
+            self.assertEqual(fromS3.read().decode('utf-8'), 'test')
+            self.amazon.delete(id)
+            self.assertFalse(self.amazon.exists(id))
+        else:
+            self.assertTrue(True)
