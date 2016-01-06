d9407fa5e9ab6a86be76dc53264ff6b6a0944956
akintolga <akint@aap.com.au>
2016-01-06 11:12:03 +1100

    [SD-3864] - Session clean-up end point

--- superdesk/users/services.py
+++ superdesk/users/services.py
@@ -10,7 +10,7 @@
 
 import flask
 import logging
-from flask import current_app as app
+from flask import request, current_app as app
 from eve.utils import config
 from superdesk.activity import add_activity, ACTIVITY_CREATE, ACTIVITY_UPDATE
 from superdesk.metadata.item import SIGN_OFF
@@ -411,3 +411,47 @@ class DBUsersService(UsersService):
 
         super().on_deleted(doc)
         get_resource_service('reset_user_password').remove_all_tokens_for_email(doc.get('email'))
+
+
+class UserSessionClearService(BaseService):
+
+    def delete(self, lookup):
+        """
+        Deletes all the records from auth and corresponding
+        session_preferences from user collections
+        If there are any orphan session_preferences exist they get deleted as well
+        """
+        users_service = get_resource_service('users')
+        user_id = request.view_args['user_id']
+        user = users_service.find_one(req=None, _id=user_id)
+        sessions = get_resource_service('auth').get(req=None, lookup={'user': user_id})
+
+        error_message = self.__can_clear_sessions(user)
+        if error_message:
+            raise SuperdeskApiError.forbiddenError(message=error_message)
+
+        # Delete all the sessions
+        for session in sessions:
+            get_resource_service('auth').delete_action({config.ID_FIELD: str(session[config.ID_FIELD])})
+
+        # Check if any orphan session_preferences exist for the user
+        if user.get('session_preferences'):
+            # Delete the orphan sessions
+            users_service.patch(user[config.ID_FIELD], {'session_preferences': {}})
+
+        return [{'complete': True}]
+
+    def __can_clear_sessions(self, user):
+        """
+        Checks if the session clear request is Invalid.
+        Operation is invalid if one of the below is True:
+            1. Check if the user exists.
+            2. Check if the user is clearing his/her own sessions.
+        :return: error message if invalid.
+        """
+
+        if not user:
+            return 'Invalid user to clear sessions.'
+
+        if str(user[config.ID_FIELD]) == str(flask.g.user[config.ID_FIELD]):
+            return 'Not allowed to clear your own sessions.'
