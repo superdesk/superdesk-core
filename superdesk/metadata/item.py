5e2dac1e207b9a2ea0919f78cf13e7917bc96d7c
Mayur Dhamanwala <mdhamanwala@aap.com.au>
2016-01-14 21:53:51 +1100

    (fix) For serialization with place fields.

--- superdesk/metadata/item.py
+++ superdesk/metadata/item.py
@@ -293,10 +293,7 @@ metadata_schema = {
     # aka Locator as per NewML Specification
     'place': {
         'type': 'list',
-        'nullable': True,
-        'schema': {
-            'type': 'dict'
-        }
+        'nullable': True
     },
 
     # Not Categorized
