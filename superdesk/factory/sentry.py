70e4cb79c76d6a0debd43fb5f98d13f26e73629d
Petr <petr.jasek@sourcefabric.org>
2016-02-09 14:35:29 +1100

    fix(sentry): make it work with new sentry
    in case sentry is not configured it throws an error in newer versions.
    this adds a proxy which will only make an instance if configured.

---
+++ superdesk/factory/sentry.py
@@ -0,0 +1,20 @@
+
+from raven.contrib.flask import Sentry
+
+
+class SuperdeskSentry():
+    """Sentry proxy that will do nothing in case sentry is not configured."""
+
+    def __init__(self, app):
+        if app.config.get('SENTRY_DSN'):
+            self.sentry = Sentry(app, register_signal=False, wrap_wsgi=False)
+        else:
+            self.sentry = None
+
+    def captureException(self, exc_info=None, **kwargs):
+        if self.sentry:
+            self.sentry.captureException(exc_info, **kwargs)
+
+    def captureMessage(self, message, **kwargs):
+        if self.sentry:
+            self.sentry.captureMessage(message, **kwargs)
