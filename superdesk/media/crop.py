3f5920696bed38498a6436905d4a7f4bcebdfe25
akintolga <akint@aap.com.au>
2016-02-16 10:49:09 +1100

    fix(crops) - Custom crop size is parsed as int

--- superdesk/media/crop.py
+++ superdesk/media/crop.py
@@ -69,13 +69,15 @@ class CropService():
 
         width = doc['CropRight'] - doc['CropLeft']
         height = doc['CropBottom'] - doc['CropTop']
+        crop_width = int(crop['width'])
+        crop_height = int(crop['height'])
 
-        if width < crop['width'] or height < crop['height']:
+        if width < crop_width or height < crop_height:
             raise SuperdeskApiError.badRequestError(
                 message='Wrong crop size. Minimum crop size is {}x{}.'.format(crop['width'], crop['height']))
 
         doc_ratio = round(width / height, 1)
-        spec_ratio = round(crop['width'] / crop['height'], 1)
+        spec_ratio = round(crop_width / crop_height, 1)
         if doc_ratio != spec_ratio:
             raise SuperdeskApiError.badRequestError(message='Wrong aspect ratio!')
 
