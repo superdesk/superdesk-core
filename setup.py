147e1acce2fa98ed937ebf472963780f35692b93
Petr <petr.jasek@sourcefabric.org>
2016-02-24 12:16:40 +0100

    fix(setup): set elasticsearch version in setup

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ LONG_DESCRIPTION = "Superdesk Server Core"
 install_requires = [
     'eve>=0.6',
     'eve-elastic>=0.3.2,<0.4',
+    'elasticsearch==1.9.0',
     'flask>=0.10',
     'flask-mail>=0.9',
     'flask-script>=2.0.5',
