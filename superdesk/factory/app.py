1787a1d5ae53a2a676570af556b3fa59aebc9d1c
Petr Jasek <jasekpetr@gmail.com>
2016-03-15 22:46:58 +0100

    fix(init): use init_index instead of put_mapping
    put_mapping would fail in case index is not there,
    while init_index can create it if missing.
    
    this was in issue on travis, after a failure when gunicorn
    created a new process it could call put_mapping during the time
    when index was removed by other gunicorn process, and a failure
    in app init stops gunicorn and thus honcho.

--- superdesk/factory/app.py
+++ superdesk/factory/app.py
@@ -114,7 +114,7 @@ def get_app(config=None, media_storage=None, config_object=None):
         app.jinja_env.filters[name] = jinja_filter
 
     # we can only put mapping when all resources are registered
-    app.data.elastic.put_mapping(app)
+    app.data.elastic.init_index(app)
 
     # instantiate registered provider classes (leave non-classes intact)
     for key, provider in registered_feeding_services.items():
