941cc01d2d3cdcd705f3dc118af96d9ec2539981
actionless <actionless.loveless@gmail.com>
2016-04-13 14:16:25 +0200

    fix(storage: amazon): don't use url_generator if proxy is unset

--- superdesk/storage/amazon/amazon_media_storage_test.py
+++ superdesk/storage/amazon/amazon_media_storage_test.py
@@ -15,22 +15,41 @@ from unittest.mock import MagicMock
 
 
 class AmazonMediaStorageTestCase(unittest.TestCase):
-    config = {
-        'AMAZON_CONTAINER_NAME': 'test_container',
-        'AMAZON_REGION': 'test_region',
-        'AMAZON_ACCESS_KEY_ID': 'test_key',
-        'AMAZON_SECRET_ACCESS_KEY': 'secret_key',
-        'AMAZON_SERVE_DIRECT_LINKS': True,
-        'AMAZON_S3_USE_HTTPS': False,
-        'AMAZON_SERVER': 'amazonaws.com',
-        'AMAZON_PROXY_SERVER': 's3.proxy.com',
-        'AMAZON_URL_GENERATOR': 'default'
-    }
+
+    def setUp(self):
+        super().setUp()
+        self.config = {
+            'AMAZON_CONTAINER_NAME': 'test_container',
+            'AMAZON_REGION': 'test_region',
+            'AMAZON_ACCESS_KEY_ID': 'test_key',
+            'AMAZON_SECRET_ACCESS_KEY': 'secret_key',
+            'AMAZON_SERVE_DIRECT_LINKS': True,
+            'AMAZON_S3_USE_HTTPS': False,
+            'AMAZON_SERVER': 'amazonaws.com',
+            'AMAZON_PROXY_SERVER': None,
+            'AMAZON_URL_GENERATOR': 'default'
+        }
 
     @mock.patch('superdesk.storage.amazon.amazon_media_storage.boto3', MagicMock())
     def test_url_for_media(self):
         app = MagicMock()
         app.config = self.config
+        url = AmazonMediaStorage(app).url_for_media('media_id.jpg')
+        self.assertEqual(url, 'http://test_container.s3-test_region.amazonaws.com/media_id.jpg')
+
+        app.config['AMAZON_S3_USE_HTTPS'] = True
+        url = AmazonMediaStorage(app).url_for_media('media_id.jpg')
+        self.assertEqual(url, 'https://test_container.s3-test_region.amazonaws.com/media_id.jpg')
+
+        app.config['AMAZON_URL_GENERATOR'] = 'partial'
+        url = AmazonMediaStorage(app).url_for_media('media_id.jpg')
+        self.assertEqual(url, 'https://test_container.s3-test_region.amazonaws.com/media_id.jpg')
+
+    @mock.patch('superdesk.storage.amazon.amazon_media_storage.boto3', MagicMock())
+    def test_url_for_media_with_proxy(self):
+        app = MagicMock()
+        app.config = self.config
+        app.config['AMAZON_PROXY_SERVER'] = 's3.proxy.com'
         url = AmazonMediaStorage(app).url_for_media('media_id.jpg')
         self.assertEqual(url, 'http://s3.proxy.com/test_container.s3-test_region.amazonaws.com/media_id.jpg')
 
