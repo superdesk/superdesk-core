d19a1507daababe736ddbad4f72b0a90e9dc3330
Petr Jasek <jasekpetr@gmail.com>
2016-04-14 15:01:53 +0200

    fix(expiry): fix check if item is expired
    there are items with `expiry` set to `None`, not sure how.
    atm remove expired command fails on master, this should fix it.

---
+++ apps/archive/commands_test.py
@@ -0,0 +1,11 @@
+
+from superdesk.tests import TestCase
+from apps.archive.commands import RemoveExpiredContent
+
+
+class RemoveExpiredContentTestCase(TestCase):
+
+    def test_is_expired(self):
+        command = RemoveExpiredContent()
+        item = {'expiry': None, '_id': 'foo'}
+        self.assertFalse(command._can_remove_item(item))
