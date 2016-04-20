bcb0d62f8c6f01f8b1f66a8ce27be6d34a511716
actionless <actionless.loveless@gmail.com>
2016-04-20 15:11:29 +0200

    fix(publish: transmitters: http): repair and improve exception message

--- superdesk/publish/transmitters/http_push.py
+++ superdesk/publish/transmitters/http_push.py
@@ -64,7 +64,10 @@ class HTTPPushService(PublishService):
                 files = {'media': (rendition['media'], media, rendition.get('mimetype') or rendition['mime_type'])}
                 response = requests.post(assets_url, files=files, data={'media_id': rendition['media']})
                 if response.status_code != requests.codes.created:  # @UndefinedVariable
-                    raise Exception('Error pushing item %s media file %s' % (item._id, rendition['media']))
+                    raise Exception('Error pushing item %s media file %s: %s %s' % (
+                        item.get("_id", ""), rendition.get('media', ""),
+                        response.status_code, response.text
+                    ))
 
     def _media_exists(self, media_id, assets_url):
         """Returns true if the media with the given id exists at the service identified by assets_url.
