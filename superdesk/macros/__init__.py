f99a51c7f4f14414208bda81719f3a0ac6438e1a
Mayur Dhamanwala <mdhamanwala@aap.com.au>
2016-05-27 15:03:38 +1000

    [SD-4611] Move AAP specific marcos to AAP branch.

--- superdesk/macros/__init__.py
+++ superdesk/macros/__init__.py
@@ -10,19 +10,28 @@ import sys
 import imp
 import importlib
 
+
+def load_macros(path, package_prefix='superdesk.macros'):
+    """
+    load macros
+    :param str path:
+    :param str package_prefix:
+    """
+    macros = [f[:-3] for f in os.listdir(path)
+              if f.endswith('.py') and not f.endswith('_test.py') and not f.startswith('__')]
+
+    for macro in macros:
+        try:
+            module = '{}.{}'.format(package_prefix, macro)
+            if module in sys.modules.keys():
+                m = sys.modules[module]
+                imp.reload(m)
+            else:
+                importlib.import_module(module)
+        except:
+            pass
+
+
 macro_replacement_fields = {'body_html', 'body_text', 'abstract', 'headline', 'slugline', 'description_text'}
 macros_folder = os.path.realpath(os.path.dirname(__file__))
-
-macros = [f[:-3] for f in os.listdir(macros_folder)
-          if f.endswith('.py') and not f.endswith('_test.py') and not f.startswith('__')]
-
-for macro in macros:
-    try:
-        module = 'superdesk.macros.{}'.format(macro)
-        if module in sys.modules.keys():
-            m = sys.modules[module]
-            imp.reload(m)
-        else:
-            importlib.import_module(module)
-    except:
-        pass
+load_macros(macros_folder)
