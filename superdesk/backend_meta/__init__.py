1f16a27bf3f90e07d2e5529c53a1c10af0e2389b
Jérôme Poisson <jerome.poisson@sourcefabric.org>
2016-07-26 18:15:32 +0200

    New endpoint to handle general metadata on backend and build informations
    This patch add a new "backend_meta" endpoint.
    It is used to give git build revision data to client, but can be used
    for other metadata in the future.
    
    SD-4770

---
+++ superdesk/backend_meta/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8; -*-
+#
+# This file is part of Superdesk.
+#
+# Copyright 2013, 2016 Sourcefabric z.u. and contributors.
+#
+# For the full copyright and license information, please see the
+# AUTHORS and LICENSE files distributed with this source code, or
+# at https://www.sourcefabric.org/superdesk/license
+
+from .backend_meta import BackendMetaResource, BackendMetaService
+import superdesk
+
+
+def init_app(app):
+    endpoint_name = 'backend_meta'
+    service = BackendMetaService(endpoint_name, backend=superdesk.get_backend())
+    BackendMetaResource(endpoint_name, app=app, service=service)
