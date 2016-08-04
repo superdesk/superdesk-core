61569ba772b222eac835ff24d0fd99509e3a2de7
marwoodandrew <amarwood@aap.com.au>
2016-08-04 11:09:12 +1000

    [SD-5135] Allow for no place and null place

--- tests/publish/email_formatter_test.py
+++ tests/publish/email_formatter_test.py
@@ -88,3 +88,76 @@ class EmailFormatterTest(SuperdeskTestCase):
         item = json.loads(doc)
         self.assertEqual(item['message_subject'], 'This is a test headline')
         self.assertEqual(item['message_html'], None)
+
+    def test_no_place_formatter(self):
+        article = {
+            'source': 'AAP',
+            'headline': 'This is a test headline',
+            'abstract': 'Can of beans',
+            'byline': 'joe',
+            'ednote': 'Very good story',
+            'dateline': {'text': 'BERN, July 13  -'},
+            'slugline': 'slugline',
+            'anpa_take_key': 'take',
+            'subject': [{'qcode': '02011001'}],
+            'format': 'HTML',
+            'type': 'text',
+            'body_html': '<p>The story body of the story so far</p>',
+            'word_count': '1',
+            'priority': 1,
+            'genre': [],
+            'sign_off': 'aa/bb'
+        }
+
+        article['versioncreated'] = datetime.datetime(year=2015, month=1, day=30, hour=2, minute=40, second=56,
+                                                      tzinfo=utc)
+        seq, doc = self.formatter.format(article, {'name': 'Test Subscriber'})[0]
+        item = json.loads(doc)
+        self.assertEqual(item['message_subject'], 'This is a test headline')
+        self.assertEqual(item['message_html'], '<html>\n<body>\n<h1>This is a test headline</h1>\n'
+                                               'Published At : Fri Jan 30 03:40:56 2015\n<br>\n<b>slugline'
+                                               '</b>&nbsp;take\n<hr>\n<br><font color="red">Very good story</font>'
+                                               '<br>\n<i>Can of beans</i>\n<br>joe\n<br>\n'
+                                               '<p>BERN, July 13  - The story body of the story so far</p>\n<br>\n'
+                                               'AAP&nbsp;aa/bb\n\n</body>\n</html>')
+        self.assertEqual(item['message_text'], 'This is a test headline\nPublished At : Fri Jan 30 03:40:56 2015\n'
+                                               'slugline take\nVery good story\n------------------------------------'
+                                               '----------------------\nCan of beans\n\njoe\nBERN, July 13 - The story '
+                                               'body of the story so far\nAAP aa/bb\n')
+
+    def test_none_place_formatter(self):
+        article = {
+            'source': 'AAP',
+            'headline': 'This is a test headline',
+            'abstract': 'Can of beans',
+            'byline': 'joe',
+            'ednote': 'Very good story',
+            'dateline': {'text': 'BERN, July 13  -'},
+            'slugline': 'slugline',
+            'anpa_take_key': 'take',
+            'subject': [{'qcode': '02011001'}],
+            'format': 'HTML',
+            'type': 'text',
+            'body_html': '<p>The story body of the story so far</p>',
+            'word_count': '1',
+            'priority': 1,
+            'place': None,
+            'genre': [],
+            'sign_off': 'aa/bb'
+        }
+
+        article['versioncreated'] = datetime.datetime(year=2015, month=1, day=30, hour=2, minute=40, second=56,
+                                                      tzinfo=utc)
+        seq, doc = self.formatter.format(article, {'name': 'Test Subscriber'})[0]
+        item = json.loads(doc)
+        self.assertEqual(item['message_subject'], 'This is a test headline')
+        self.assertEqual(item['message_html'], '<html>\n<body>\n<h1>This is a test headline</h1>\n'
+                                               'Published At : Fri Jan 30 03:40:56 2015\n<br>\n<b>slugline'
+                                               '</b>&nbsp;take\n<hr>\n<br><font color="red">Very good story</font>'
+                                               '<br>\n<i>Can of beans</i>\n<br>joe\n<br>\n'
+                                               '<p>BERN, July 13  - The story body of the story so far</p>\n<br>\n'
+                                               'AAP&nbsp;aa/bb\n\n</body>\n</html>')
+        self.assertEqual(item['message_text'], 'This is a test headline\nPublished At : Fri Jan 30 03:40:56 2015\n'
+                                               'slugline take\nVery good story\n------------------------------------'
+                                               '----------------------\nCan of beans\n\njoe\nBERN, July 13 - The story '
+                                               'body of the story so far\nAAP aa/bb\n')
