7e7b3cc88ae7bad9bf233f6fc0ff35708d83297e
Petr <petr.jasek@sourcefabric.org>
2016-08-24 12:51:25 +0200

    release 1.1rc1

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ package_data = {
 
 setup(
     name='Superdesk-Core',
-    version='1.0',
+    version='1.1rc1',
     description='Superdesk Core library',
     long_description=LONG_DESCRIPTION,
     author='petr jasek',
