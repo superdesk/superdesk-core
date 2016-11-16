ca7bbb59c6cb5271250e8d48c9563731bee7879f
Anca Farcas <anca.farcas@sourcefabric.org>
2016-11-16 11:13:48 +0200

    name changes and filter conditions added

--- apps/analytics/__init__.py
+++ apps/analytics/__init__.py
@@ -1,11 +1,11 @@
 import superdesk
-from apps.analytics.activityreports import ActivityReportResource, ActivityReportService
+from apps.analytics.activity_reports import ActivityReportResource, ActivityReportService
 
 
 def init_app(app):
-    endpoint_name = 'activityreports'
+    endpoint_name = 'activity_reports'
     service = ActivityReportService(endpoint_name, backend=superdesk.get_backend())
     ActivityReportResource(endpoint_name, app=app, service=service)
 
-    superdesk.privilege(name='activityreports', label='Activity Report View',
+    superdesk.privilege(name='activity_reports', label='Activity Report View',
                         description='User can view activity reports.')
