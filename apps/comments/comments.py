a44075acf2856c6a9865700fad495baaca525e67
Petr <petr.jasek@sourcefabric.org>
2016-12-19 13:08:56 +0100

    fix(comments): fix user mentions if there is `.` in the name
    there is a workaround for mongo which can't have `.` in dict keys
    
    SDESK-472

--- apps/comments/comments.py
+++ apps/comments/comments.py
@@ -42,6 +42,36 @@ class CommentsResource(Resource):
     privileges = {'POST': 'archive', 'DELETE': 'archive'}
 
 
+def encode_keys(doc, field):
+    """Encode field keys before storing.
+
+    Mongo doesn't allow `.` in dict key.
+
+    :param doc: doc
+    :param field: doc field
+    """
+    if not doc.get(field):
+        return
+    encoded = {}
+    for key, val in doc.get(field).items():
+        encoded[key.replace('.', ';')] = val
+    doc[field] = encoded
+
+
+def decode_keys(doc, field):
+    """Decode encoded field keys.
+
+    :param doc: doc
+    :param field: doc field
+    """
+    if not doc.get(field):
+        return
+    decoded = {}
+    for key, val in doc.get(field).items():
+        decoded[key.replace(';', '.')] = val
+    doc[field] = decoded
+
+
 class CommentsService(BaseService):
     notification_key = 'comments'
 
@@ -56,10 +86,19 @@ class CommentsService(BaseService):
             user_names, desk_names = get_mentions(doc.get('text'))
             doc['mentioned_users'] = get_users(user_names)
             doc['mentioned_desks'] = get_desks(desk_names)
+            encode_keys(doc, 'mentioned_users')
+            encode_keys(doc, 'mentioned_desks')
+
+    def on_fetched(self, doc):
+        for item in doc.get('_items', []):
+            decode_keys(item, 'mentioned_users')
+            decode_keys(item, 'mentioned_desks')
 
     def on_created(self, docs):
         for doc in docs:
             push_notification(self.notification_key, item=str(doc.get('item')))
+            decode_keys(doc, 'mentioned_users')
+            decode_keys(doc, 'mentioned_desks')
 
         notify_mentioned_users(docs, app.config.get('CLIENT_URL', ''))
         notify_mentioned_desks(docs)
