4f22e9ea3910e1d069a0a7cb73c466e5459d5b74
mugurrus <mugur.rus@sourcefabric.org>
2017-03-24 14:29:06 +0200

    fixes for adding items to published packages (#857)

--- apps/publish/content/published_package_items.py
+++ apps/publish/content/published_package_items.py
@@ -19,18 +19,16 @@ from apps.packages.package_service import PackageService, create_root_group,\
 from eve.utils import config
 from eve.validation import ValidationError
 from superdesk.errors import SuperdeskApiError
-from superdesk.metadata.utils import item_url
 from superdesk.services import BaseService
 from superdesk.metadata.packages import GROUPS, GROUP_ID, REFS, RESIDREF
 from copy import deepcopy
 
 
 class PublishedPackageItemsResource(Resource):
-    item_url = item_url
     schema = {
-        config.ID_FIELD: {
+        'package_id': {
             'type': 'string',
-            'unique': True
+            'required': True
         },
         'new_items': {
             'type': 'list',
@@ -47,39 +45,42 @@ class PublishedPackageItemsResource(Resource):
     datasource = {
         'source': 'archive'
     }
-    item_methods = ['PATCH']
-    privileges = {'PATCH': ARCHIVE}
+    resource_methods = ['POST']
+    privileges = {'POST': ARCHIVE}
 
 
 class PublishedPackageItemsService(BaseService):
     package_service = PackageService()
 
-    def update(self, id, updates, original):
-        original = get_resource_service(ARCHIVE).find_one(req=None, _id=id)
-        if not original or original[ITEM_TYPE] != CONTENT_TYPE.COMPOSITE:
-            raise SuperdeskApiError.badRequestError('Invalid package identifier')
-        if original[ITEM_STATE] not in PUBLISH_STATES:
-            raise SuperdeskApiError.badRequestError('Package was not published')
-        items = {}
-        for new_item in updates['new_items']:
-            item = get_resource_service(ARCHIVE).find_one(req=None, _id=new_item['item_id'])
-            if not item:
-                raise SuperdeskApiError.badRequestError('Invalid item identifier %s' % new_item['item_id'])
-            try:
-                self.package_service.check_for_circular_reference(original, new_item['item_id'])
-            except ValidationError:
-                raise SuperdeskApiError.badRequestError('Circular reference in item %s', new_item['item_id'])
-            items[item[config.ID_FIELD]] = item
-
-        package = deepcopy(original)
-        items_refs = []
-        for new_item in updates['new_items']:
-            create_root_group([package])
-            items_refs.append(self._set_item_assoc(package, new_item, items[new_item['item_id']]))
-        get_resource_service(ARCHIVE).system_update(id, package, original)
-        for item_ref in items_refs:
-            self.package_service.update_link(package, item_ref)
-        return package
+    def create(self, docs, **kwargs):
+        ids = []
+        for doc in docs:
+            original = get_resource_service(ARCHIVE).find_one(req=None, _id=doc['package_id'])
+            if not original or original[ITEM_TYPE] != CONTENT_TYPE.COMPOSITE:
+                raise SuperdeskApiError.badRequestError('Invalid package identifier')
+            if original[ITEM_STATE] not in PUBLISH_STATES:
+                raise SuperdeskApiError.badRequestError('Package was not published')
+            items = {}
+            for new_item in doc['new_items']:
+                item = get_resource_service(ARCHIVE).find_one(req=None, _id=new_item['item_id'])
+                if not item:
+                    raise SuperdeskApiError.badRequestError('Invalid item identifier %s' % new_item['item_id'])
+                try:
+                    self.package_service.check_for_circular_reference(original, new_item['item_id'])
+                except ValidationError:
+                    raise SuperdeskApiError.badRequestError('Circular reference in item %s', new_item['item_id'])
+                items[item[config.ID_FIELD]] = item
+    
+            package = deepcopy(original)
+            items_refs = []
+            for new_item in doc['new_items']:
+                create_root_group([package])
+                items_refs.append(self._set_item_assoc(package, new_item, items[new_item['item_id']]))
+            get_resource_service(ARCHIVE).system_update(package[config.ID_FIELD], package, original)
+            for item_ref in items_refs:
+                self.package_service.update_link(package, item_ref)
+            ids.append(package[config.ID_FIELD])
+        return ids
 
     def _set_item_assoc(self, package, new_item, item_doc):
         items_refs = []
