5d858721063cf8c845fc100225281af6dffd0542
Petr <petr.jasek@sourcefabric.org>
2017-04-12 13:06:29 +0200

    release 1.7rc2

--- superdesk/__init__.py
+++ superdesk/__init__.py
@@ -25,7 +25,7 @@ from .resource import Resource  # noqa
 from .privilege import privilege, intrinsic_privilege, get_intrinsic_privileges  # noqa
 from .workflow import *  # noqa
 
-__version__ = '1.7rc1'
+__version__ = '1.7rc2'
 
 API_NAME = 'Superdesk API'
 SCHEMA_VERSION = 0
