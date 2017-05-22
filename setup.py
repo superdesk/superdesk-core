a57526dcb4f3f6b866b7b6d73d663b77c207a115
petr <jasekpetr@gmail.com>
2017-05-22 12:58:15 +0200

    release 1.7

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ package_data = {
 
 setup(
     name='Superdesk-Core',
-    version='1.7rc8',
+    version='1.7',
     description='Superdesk Core library',
     long_description=LONG_DESCRIPTION,
     author='petr jasek',
