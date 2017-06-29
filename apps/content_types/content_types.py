2947b674638215df0e51202f6a09896d4b1aed28
Ioan v. Pocol <ioan.pocol@sourcefabric.org>
2017-06-29 12:50:31 +0300

    feat(content profile): Add support for usage terms on content profile

--- apps/content_types/content_types.py
+++ apps/content_types/content_types.py
@@ -25,6 +25,9 @@ class DefaultSchema(schema.Schema):
     #: language
     language = schema.StringField()
 
+    #: usageterms
+    usageterms = schema.StringField()
+
     #: slugline
     slugline = schema.StringField(maxlength=24)
 
@@ -109,6 +112,7 @@ DEFAULT_EDITOR = {
     'slugline': {'order': 0, 'sdWidth': 'full', 'enabled': True},
     'keywords': {'order': 1, 'sdWidth': 'full', 'enabled': False},
     'language': {'order': 1, 'sdWidth': 'half', 'enabled': False},
+    'usageterms': {'order': 1, 'sdWidth': 'full', 'enabled': False},
     'genre': {'order': 2, 'sdWidth': 'half', 'enabled': True},
     'anpa_take_key': {'order': 3, 'sdWidth': 'half', 'enabled': False},
     'place': {'order': 4, 'sdWidth': 'half', 'enabled': True},
