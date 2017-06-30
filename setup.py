e58beea3b82e22f83ff65c3de85d1b44ad98e6f0
Petr <petr.jasek@sourcefabric.org>
2017-06-30 14:53:47 +0200

    release 1.8.4

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ install_requires = [
     'websockets>=3.0,<3.3',
     'mongolock>=1.3.4,<1.4',
     'PyYAML>=3.11,<3.13',
-    'lxml',
+    'lxml>=3.7.3,<3.8',
 ]
 
 package_data = {
@@ -60,7 +60,7 @@ package_data = {
 
 setup(
     name='Superdesk-Core',
-    version='1.8.3',
+    version='1.8.4',
     description='Superdesk Core library',
     long_description=LONG_DESCRIPTION,
     author='petr jasek',
