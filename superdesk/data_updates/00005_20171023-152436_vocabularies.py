b38173f632358323cb5d5fc03a12ed18437b0197
Jérôme Poisson <jerome.poisson@sourcefabric.org>
2017-10-24 10:27:27 +0200

    [DATA] [VOCABULARIES]: update file to set unique_field for author_roles and  job_titles
    SDESK-1984

---
+++ superdesk/data_updates/00005_20171023-152436_vocabularies.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8; -*-
+# This file is part of Superdesk.
+# For the full copyright and license information, please see the
+# AUTHORS and LICENSE files distributed with this source code, or
+# at https://www.sourcefabric.org/superdesk/license
+#
+# Author  : Jérôme
+# Creation: 2017-10-23 15:24
+
+from superdesk.commands.data_updates import DataUpdate
+
+
+class DataUpdate(DataUpdate):
+
+    resource = 'vocabularies'
+
+    def forwards(self, mongodb_collection, mongodb_database):
+        print(mongodb_collection.update_many({'_id': {'$in': ['author_roles', 'job_titles']}},
+                                             {'$set': {
+                                                 'unique_field': "qcode"
+                                             }}))
+
+    def backwards(self, mongodb_collection, mongodb_database):
+        print(mongodb_collection.update_many({'_id': {'$in': ['author_roles', 'job_titles']}},
+                                             {'$unset': {
+                                                 'unique_field': "qcode"
+                                             }}))
