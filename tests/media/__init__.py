8b693be669c53466a4f134ac4439a00374b992ef
Petr <petr.jasek@sourcefabric.org>
2018-02-12 14:49:11 +0100

    fix(crop): avoid dark border on picture crops
    - it could be using larger crop area than what was available and
    thus adding a few extra dark pixels
    
    SDESK-2415

--- tests/media/__init__.py
+++ tests/media/__init__.py
@@ -2,6 +2,6 @@
 import os
 
 
-def get_picture_fixture():
+def get_picture_fixture(filename='iphone_gpsinfo_exif.JPG'):
     """Get picture file path."""
-    return os.path.join(os.path.dirname(os.path.realpath(__file__)), 'fixtures', 'iphone_gpsinfo_exif.JPG')
+    return os.path.join(os.path.dirname(os.path.realpath(__file__)), 'fixtures', filename)
