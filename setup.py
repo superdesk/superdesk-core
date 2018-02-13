e1c6f8d9b405c69dd7339b358142756ad0ee3efc
Petr Jasek <jasekpetr@gmail.com>
2018-02-13 14:09:51 +0100

    chore(elastic): update eve-elastic dependency
    - it won't close/open elastic index when settings are already set

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ LONG_DESCRIPTION = "Superdesk Server Core"
 
 install_requires = [
     'eve>=0.6,<=0.7.2',
-    'eve-elastic==2.3',
+    'eve-elastic==2.4.1',
     'flask>=0.10.1,<=0.12',
     'flask-oauthlib>=0.9.3,<0.10',
     'flask-mail>=0.9,<0.10',
