3a7f3063df5964480197f63c3dd32ac1fb2d7aa6
tomas <t.kikutis@gmail.com>
2018-03-23 08:20:22 +0200

    * Use `with` for managing subprocesses * Remove `return` left from previous testing

--- superdesk/data_updates/00007_20180321-092824_archive.py
+++ superdesk/data_updates/00007_20180321-092824_archive.py
@@ -13,23 +13,14 @@ import json
 from superdesk.commands.data_updates import DataUpdate
 from os.path import realpath, join, dirname
 
+node_script_path = join(dirname(realpath(superdesk.__file__)), "data_updates", "00007_20180321-092824_archive.dist.js")
+
 
 def get_updated_editor_state(editor_state):
     try:
-        p1 = subprocess.Popen(["echo", json.dumps(editor_state)], stdout=subprocess.PIPE)
-        p2 = subprocess.Popen(
-            [
-                "node",
-                join(dirname(realpath(superdesk.__file__)), "data_updates", "00007_20180321-092824_archive.dist.js")
-            ],
-            stdin=p1.stdout, stdout=subprocess.PIPE
-        )
-
-        p1.stdout.close()  # Allow p1 to receive a SIGPIPE if p2 exits.
-        output, err = p2.communicate()
-
-        return json.loads(output.decode("utf-8"))
-
+        with subprocess.Popen(['node', node_script_path], stdin=subprocess.PIPE, stdout=subprocess.PIPE) as p:
+            output, err = p.communicate(bytes(json.dumps(editor_state), 'utf-8'))
+            return json.loads(output.decode('utf-8'))
     except Exception:
         return editor_state
 
@@ -39,10 +30,7 @@ class DataUpdate(DataUpdate):
     resource = 'archive'
 
     def forwards(self, mongodb_collection, mongodb_database):
-        return
-
         for item in mongodb_collection.find({'editor_state': {'$exists': True}}):
-
             print(mongodb_collection.update({'_id': item['_id']}, {
                 '$set': {
                     'editor_state': get_updated_editor_state(item['editor_state'])
