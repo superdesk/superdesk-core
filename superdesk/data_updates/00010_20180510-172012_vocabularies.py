8762a1aba90fb4fb03c25e0b7d7bf8403ed99b2f
Jérôme Poisson <jerome.poisson@sourcefabric.org>
2018-05-10 18:30:24 +0200

    [VOCABULARIES] set qcode as unique_field for 'genre', 'priority', 'replace_words' and 'annotation_types'
    this avoid the use of empty qcode for those data
    
    SDESK-2763

---
+++ superdesk/data_updates/00010_20180510-172012_vocabularies.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8; -*-
+# This file is part of Superdesk.
+# For the full copyright and license information, please see the
+# AUTHORS and LICENSE files distributed with this source code, or
+# at https://www.sourcefabric.org/superdesk/license
+#
+# Author  : Jérôme
+# Creation: 2018-05-10 17:20
+
+from superdesk.commands.data_updates import DataUpdate
+
+
+class DataUpdate(DataUpdate):
+
+    resource = 'vocabularies'
+
+    def forwards(self, mongodb_collection, mongodb_database):
+        print(mongodb_collection.update_many({'_id': {'$in': ['genre',
+                                                              'priority',
+                                                              'replace_words',
+                                                              'annotation_types']}},
+                                             {'$set': {
+                                                 'unique_field': "qcode"
+                                             }}))
+
+    def backwards(self, mongodb_collection, mongodb_database):
+        print(mongodb_collection.update_many({'_id': {'$in': ['genre',
+                                                              'priority',
+                                                              'replace_words',
+                                                              'annotation_types']}},
+                                             {'$unset': {
+                                                 'unique_field': "qcode"
+                                             }}))
