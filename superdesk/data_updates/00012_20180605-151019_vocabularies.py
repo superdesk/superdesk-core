4048a6801c6d59215e05d66cf5d5fdd13a699c9c
Mugur Rus <mugur.rus@sourcefabric.org>
2018-06-06 01:12:51 +0300

    specify qcode type for priority and urgency vocabularies

---
+++ superdesk/data_updates/00012_20180605-151019_vocabularies.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8; -*-
+# This file is part of Superdesk.
+# For the full copyright and license information, please see the
+# AUTHORS and LICENSE files distributed with this source code, or
+# at https://www.sourcefabric.org/superdesk/license
+#
+# Author  : mugur
+# Creation: 2018-06-05 15:10
+
+from eve.utils import config
+from superdesk.commands.data_updates import DataUpdate
+
+
+class DataUpdate(DataUpdate):
+
+    resource = 'vocabularies'
+
+    def forwards(self, mongodb_collection, mongodb_database):
+        for vocabulary in mongodb_collection.find({'_id': {'$in': ['priority', 'urgency']}}):
+            schema = vocabulary.get('schema', {})
+            qcode = schema.get('qcode', {})
+            qcode['type'] = 'integer'
+            schema['qcode'] = qcode
+            print(mongodb_collection.update({'_id': vocabulary.get(config.ID_FIELD)},
+                                            {'$set': {'schema': schema}}))
+
+    def backwards(self, mongodb_collection, mongodb_database):
+        pass
