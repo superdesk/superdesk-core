8bfadbaf45b8a5985ec1f554ea0f4ebd063fa182
tomas <t.kikutis@gmail.com>
2018-06-27 09:40:18 +0300

    Log item JSON in case there's an exception

--- superdesk/data_updates/00013_20180618-090440_archive.py
+++ superdesk/data_updates/00013_20180618-090440_archive.py
@@ -21,34 +21,42 @@ class DataUpdate(DataUpdate):
             collection = mongodb_database[resource]
 
             for item in collection.find({'fields_meta': {"$gt": {}}}):
-                for field_meta_name in item.get('fields_meta', {}):
-                    draft_js_state = item['fields_meta'].get(field_meta_name, {}).get('draftjsState', [])
-                    entity_map = draft_js_state[0].get('entityMap', {}) if len(draft_js_state) > 0 else {}
 
-                    for entity_key in entity_map:
-                        entity = entity_map[entity_key]
-                        entity_data = entity.get('data', {}).get('data', {})
-                        selector = entity_data.get('selector', None)
+                try:
+                    for field_meta_name in item.get('fields_meta', {}):
+                        draft_js_state = item['fields_meta'].get(field_meta_name, {}).get('draftjsState', [])
+                        entity_map = draft_js_state[0].get('entityMap', {}) if len(draft_js_state) > 0 else {}
 
-                        if(selector is not None and selector.startswith('#qumu')):
-                            entity_map[entity_key] = {
-                                'type': 'EMBED',
-                                'mutability': 'MUTABLE',
-                                'data': {
+                        for entity_key in entity_map:
+                            entity = entity_map[entity_key]
+                            entity_data = entity.get('data', {}).get('data', {})
+                            selector = entity_data.get('selector', None)
+
+                            if(selector is not None and selector.startswith('#qumu')):
+                                entity_map[entity_key] = {
+                                    'type': 'EMBED',
+                                    'mutability': 'MUTABLE',
                                     'data': {
-                                        'html': '<script type="text/javascript"' +
-                                        ' src="https://video.fidelity.tv/widgets/application.js">' +
-                                        '</script><script type="text/javascript">' +
-                                        'KV.widget(' + json.dumps(entity_data) + ');</script>'
+                                        'data': {
+                                            'html': '<script type="text/javascript"' +
+                                            ' src="https://video.fidelity.tv/widgets/application.js">' +
+                                            '</script><script type="text/javascript">' +
+                                            'KV.widget(' + json.dumps(entity_data) + ');</script>'
+                                        }
                                     }
                                 }
-                            }
 
-                    print(collection.update({'_id': item['_id']}, {
-                        '$set': {
-                            'fields_meta.' + field_meta_name + '.draftjsState.0.entityMap': entity_map
-                        }
-                    }))
+                        print(collection.update({'_id': item['_id']}, {
+                            '$set': {
+                                'fields_meta.' + field_meta_name + '.draftjsState.0.entityMap': entity_map
+                            }
+                        }))
+                except:
+                    print('Exception occured while running an upgrade script 00013_20180618-090440_archive.py')
+                    print('Find the offending item below:\n\n')
+                    print(json.dumps(item))
+                    print('\n\n-- item end --\n\n')
+                    raise
 
     def backwards(self, mongodb_collection, mongodb_database):
         pass
