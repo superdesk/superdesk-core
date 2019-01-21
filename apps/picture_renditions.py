29c5857e564ba2300dc1df23c30254181cd709ae
GyanP <gyanig72@gmail.com>
2019-01-21 16:08:42 +0530

    (SDESK-3824)- fixed: After cropping image in edit image, other operations bring back the original

--- apps/picture_renditions.py
+++ apps/picture_renditions.py
@@ -3,6 +3,7 @@ import superdesk
 from flask import current_app as app
 from superdesk.media.renditions import generate_renditions, get_renditions_spec
 from apps.picture_crop import get_file
+from superdesk import get_resource_service
 
 
 class PictureRenditionsService(superdesk.Service):
@@ -25,8 +26,11 @@ class PictureRenditionsService(superdesk.Service):
             media_type, content_type = mimetype.split('/')
             renditions = generate_renditions(orig_file, orig['media'], inserted, media_type,
                                              mimetype, rendition_spec, app.media.url_for_media)
+
             doc['renditions'] = renditions
             ids.append(item['_id'])
+            updates = {'renditions': item['renditions']}
+            get_resource_service('archive').update(item['_id'], updates, item)
         return ids
 
 
