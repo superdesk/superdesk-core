472cdfa6d16424bf37260d87e4f2607ce2eee7af
Jérôme Poisson <jerome.poisson@sourcefabric.org>
2019-05-28 12:44:41 +0200

    [SPELLCHECKERS] Spellcheckers integration + Grammelecte implemention
    This patch introduce a generic way to register spellcheckers or tools to check
    grammar/style.
    
    2 new endpoints are available:
    
    - spellcheckers_list, to retrieve list of registered spellcheckers
    - spellchecker, to check a text
    
    The later expects a spellchecker name to be specified using `spellchecker` key,
    and a text to be specified using `text` key.
    
    The returned dict will contain an "errors" key, with a list of dict containing
    the following keys:
    - `startOffset`: offset of the offending text
    - `suggestions` (list of dicts)
    - `text`: offending text
    - `type`: one of "grammar" or "spelling"
    
    alternatively, `suggestions` boolean can be set, in which case only
    suggestions are returned (in `suggestions` key).
    
    A first module implement this API for `Grammalecte` grammar + spelling
    checker for French.
    
    SDBELGA-109

---
+++ superdesk/text_checkers/spellcheckers/base.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8; -*-
+#
+# This file is part of Superdesk.
+#
+# Copyright 2013-2019 Sourcefabric z.u. and contributors.
+#
+# For the full copyright and license information, please see the
+# AUTHORS and LICENSE files distributed with this source code, or
+# at https://www.sourcefabric.org/superdesk/license
+
+import abc
+import logging
+from collections import namedtuple
+
+logger = logging.getLogger(__name__)
+
+Spellchecker = namedtuple("Spellchecker", ["name"])
+registered_spellcheckers = {}
+
+
+class SpellcheckerCapacities:
+    all_capacities = {'spelling', 'grammar'}
+
+    def __init__(self, capacities):
+        if isinstance(capacities, str):
+            capacities = {capacities}
+        capacities = set(capacities)
+        if not capacities <= self.all_capacities:
+            raise ValueError("invalid capacities: {invalid}".format(
+                invalid=capacities - self.all_capacities))
+        self.capacities = capacities
+
+    def serialize(self):
+        return sorted(self.capacities)
+
+
+class SpellcheckerRegisterer(abc.ABCMeta):
+
+    def __call__(cls, *args, **kwargs):
+        instance = super().__call__(*args, **kwargs)
+        name = instance.name
+        if name in registered_spellcheckers:
+            raise ValueError('"{name}" spellchecker is already registered'.format(name=name))
+
+        instance.capacities = SpellcheckerCapacities(instance.capacities)
+        if not instance.available():
+            logger.warning('"{name}" spellchecker is not available'.format(name=name))
+            return None
+        registered_spellcheckers[name] = instance
+        return instance
+
+
+class SpellcheckerBase(metaclass=SpellcheckerRegisterer):
+    """Base class for spellchecker.
+
+    This class define the attribute and methods mandatory to implement.
+    Spellcheckers are automatically instanciated and registered, you just need to
+    inherit from this class in your module.
+    "label" attribute can be used, if it is not present the name will be used as label
+    """
+
+    #: what this spellchecker can do (spelling, grammar)
+    capacities = "spelling"
+
+    #: version of the spellchecker, None if unknown
+    version = None
+
+    def __init__(self, app):
+        self.config = app.config
+
+    @property
+    @abc.abstractmethod
+    def name(self):
+        pass
+
+    @property
+    def label(self):
+        return self.name.title()
+
+    @property
+    @abc.abstractmethod
+    def languages(self):
+        """List of RFC-5646 tags for languages supported by this spellchecker"""
+        pass
+
+    @abc.abstractmethod
+    def check(self, text):
+        """Check spelling is given text"""
+        pass
+
+    def suggest(self, text):
+        """Get suggestions to correct given text"""
+        logger.debug(u'"suggest" is not implemented')
+        return {'suggestions': []}
+
+    def available(self):
+        """Return True if the spellchecker is available
+
+        override this method is spellcheck availability depends of something (e.g. a
+        server launched)
+        """
+        return True
+
+    def serialize(self):
+        data = {
+            'name': self.name,
+            'label': self.label,
+            'capacities': self.capacities.serialize(),
+            'languages': self.languages,
+        }
+        if self.version is not None:
+            data['version'] = self.version
+        return data
