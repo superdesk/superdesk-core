9218796187302b0382e7abfe70d29cac8434efdf
Petr Jašek <jasekpetr@gmail.com>
2019-08-02 16:24:58 +0200

    fix related items reference in associations (#1641)
    * fix related items reference in associations
    
    we should only store _id and type for superdesk items,
    not the full item.
    
    including upgrade script to fix archive/published items.
    
    SDESK-4574

---
+++ superdesk/data_updates/00018_20190801-145234_archive.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8; -*-
+# This file is part of Superdesk.
+# For the full copyright and license information, please see the
+# AUTHORS and LICENSE files distributed with this source code, or
+# at https://www.sourcefabric.org/superdesk/license
+#
+# Author  : petr
+# Creation: 2019-08-01 14:52
+
+from superdesk import get_resource_service
+from superdesk.commands.data_updates import DataUpdate
+
+
+class DataUpdate(DataUpdate):
+
+    resource = 'archive'
+
+    def forwards(self, mongodb_collection, mongodb_database):
+        related = get_resource_service('vocabularies').get(req=None, lookup={'field_type': 'related_content'})
+        archive_service = get_resource_service('archive')
+        for resource in ('archive', 'published'):
+            service = get_resource_service(resource)
+            for item in mongodb_database[resource].find({'type': 'text', 'associations': {'$gt': {}}}):
+                update = False
+                associations = {}
+                for key, val in item['associations'].items():
+                    if val and archive_service._is_related_content(key, related) and len(val.keys()) > 2:
+                        update = True
+                        associations[key] = {
+                            '_id': val.get('item_id') or val.get('_id'),
+                            'type': val['type'],
+                        }
+                    elif val and val.get('_id') and len(val.keys()) == 1:
+                        type_ = mongodb_database[resource].find_one({'_id': val['_id']}, {'type': 1})
+                        if type_:
+                            update = True
+                            associations[key] = val
+                            associations[key]['type'] = type_['type']
+                    else:
+                        associations[key] = val
+                if update:
+                    service.system_update(item['_id'], {'associations': associations}, item)
+
+    def backwards(self, mongodb_collection, mongodb_database):
+        pass
