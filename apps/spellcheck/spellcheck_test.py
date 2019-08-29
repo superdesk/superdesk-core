c45445a9ef5326e19f4aafa355eb5ef396b48090
GyanP <gyanig72@gmail.com>
2019-08-29 11:53:13 +0530

    added test case

--- apps/spellcheck/spellcheck_test.py
+++ apps/spellcheck/spellcheck_test.py
@@ -11,3 +11,13 @@ class SpellcheckTestCase(unittest.TestCase):
         model = {'foe': 3, 'fox': 5}
         suggestions = norvig_suggest('foo', model)
         self.assertEquals(['fox', 'foe'], suggestions)
+
+    def test_suggestsions_case_insensitive(self):
+        """Test case for getting the suggestions irrespective of the case(lower/upper) of the word"""
+
+        model = {'Foe': 1, 'fox': 5}
+        suggestions_lower = norvig_suggest('fooe', model)
+        self.assertEquals(['foe'], suggestions_lower)
+
+        suggestions_upper = norvig_suggest('Fooe', model)
+        self.assertEquals(['foe'], suggestions_upper)
