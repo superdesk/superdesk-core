36f2f0dd339f0dbde60b389433c8533bcc1f3eec
olegpshenichniy <olegpshenichniy@gmail.com>
2019-09-05 11:21:43 +0200

    Reuse set_error_handlers between subapps.

--- content_api/app/__init__.py
+++ content_api/app/__init__.py
@@ -23,42 +23,14 @@ import flask
 import importlib
 
 from eve import Eve
-from eve.render import send_response
 from eve.io.mongo.mongo import MongoJSONEncoder
 
 from content_api.tokens import SubscriberTokenAuth
 from superdesk.datalayer import SuperdeskDataLayer
-from superdesk.errors import SuperdeskError, SuperdeskApiError
 from superdesk.storage import SuperdeskGridFSMediaStorage
 from superdesk.validator import SuperdeskValidator
+from superdesk.factory.app import set_error_handlers
 from superdesk.factory.sentry import SuperdeskSentry
-
-
-def _set_error_handlers(app):
-    """Set error handlers for the given application object.
-
-    Each error handler receives a :py:class:`superdesk.errors.SuperdeskError`
-    instance as a parameter and returns a tuple containing an error message
-    that is sent to the client and the HTTP status code.
-
-    :param app: an instance of `Eve <http://python-eve.org/>`_ application
-    """
-
-    # TODO: contains the same bug as the client_error_handler of the main
-    # superdesk app, fix it when the latter gets resolved (or, perhaps,
-    # replace it with a new 500 error handler tailored for the public API app)
-    @app.errorhandler(SuperdeskError)
-    def client_error_handler(error):
-        error_dict = error.to_dict()
-        error_dict.update(internal_error=error.status_code)
-        status_code = error.status_code or 422
-        return send_response(None, (error_dict, None, None, status_code))
-
-    @app.errorhandler(500)
-    def server_error_handler(error):
-        """Log server errors."""
-        return_error = SuperdeskApiError.internalError(error)
-        return client_error_handler(return_error)
 
 
 def get_app(config=None):
@@ -103,7 +75,7 @@ def get_app(config=None):
         validator=SuperdeskValidator
     )
 
-    _set_error_handlers(app)
+    set_error_handlers(app)
 
     for module_name in app.config.get('CONTENTAPI_INSTALLED_APPS', []):
         app_module = importlib.import_module(module_name)
