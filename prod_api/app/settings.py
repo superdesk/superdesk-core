403aab3ab320ca367e4125ca3217f9dc77f10a42
olegpshenichniy <olegpshenichniy@gmail.com>
2019-10-01 12:35:53 +0200

    SDBELGA-185 Production API updates.

--- prod_api/app/settings.py
+++ prod_api/app/settings.py
@@ -44,10 +44,13 @@ PRODAPI_INSTALLED_APPS = (
 
 # NOTE: no trailing slash for the PRODAPI_URL setting!
 PRODAPI_URL = env('PRODAPI_URL', 'http://localhost:5500')
-URL_PREFIX = env('PRODAPI_URL_PREFIX', 'api')
+URL_PREFIX = env('PRODAPI_URL_PREFIX', 'prodapi')
 API_VERSION = 'v1'
 MEDIA_PREFIX = env('MEDIA_PREFIX', '{}/{}/{}/assets'.format(PRODAPI_URL.rstrip('/'), URL_PREFIX, API_VERSION))
 
 # date formats
 DATE_FORMAT = '%Y-%m-%dT%H:%M:%S+0000'
 ELASTIC_DATE_FORMAT = '%Y-%m-%d'
+
+# response in json
+XML = False
