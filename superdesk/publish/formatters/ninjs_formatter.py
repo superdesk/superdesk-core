2e206e75332ed21644f85dab2401576e0a09cf83
mehdi dridi <5521531+mehdignu@users.noreply.github.com>
2019-12-02 12:50:29 +0100

    Update ninjs_formatter.py

--- superdesk/publish/formatters/ninjs_formatter.py
+++ superdesk/publish/formatters/ninjs_formatter.py
@@ -250,7 +250,7 @@ class NINJSFormatter(Formatter):
 
         if 'extra' in ninjs:
             if article.get('flags', {}).get('change_types_tmp') == "1":
-                ninjs["extra"].update({"dont_update_date": True})
+                ninjs["extra"].update({"update_date": True})
 
         if (article.get('schedule_settings') or {}).get('utc_publish_schedule'):
             ninjs['publish_schedule'] = article['schedule_settings']['utc_publish_schedule']
