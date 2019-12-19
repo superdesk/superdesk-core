1c4913042ba6d61ffdbf207ac3c184563b85b68e
Petr Jašek <jasekpetr@gmail.com>
2019-12-19 11:50:30 +0100

    move tansa config to server (#1759)
    in order to be able to use env vars for keys
    
    SDCP-70

--- apps/spellcheck/__init__.py
+++ apps/spellcheck/__init__.py
@@ -1,8 +1,10 @@
 
 from .spellcheck import SpellcheckService, SpellcheckResource
+from .tansa import init_app as init_tansa
 
 
 def init_app(app):
     endpoint_name = 'spellcheck'
     service = SpellcheckService(endpoint_name, backend=None)
     SpellcheckResource(endpoint_name, app=app, service=service)
+    init_tansa(app)
