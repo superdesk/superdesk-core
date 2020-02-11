5c126a43b9bde15fbe7e81e20bff0cc8b05a53ff
Petr Jasek <jasekpetr@gmail.com>
2020-02-11 09:50:11 +0100

    release 1.33rc1

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ package_data = {
 
 setup(
     name='Superdesk-Core',
-    version='1.32.2',
+    version='1.33rc1',
     description='Superdesk Core library',
     long_description=LONG_DESCRIPTION,
     author='petr jasek',
