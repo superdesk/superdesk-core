396ed8bc79fc9827309392f3299238d2cb6d6a47
GyanP <gyanig72@gmail.com>
2020-03-26 10:46:04 +0530

    'TIPS ingest macro' should also change content profile. [SDBELGA-304]

--- apps/duplication/archive_fetch.py
+++ apps/duplication/archive_fetch.py
@@ -101,7 +101,7 @@ class FetchService(BaseService):
 
             desk = get_resource_service('desks').find_one(req=None, _id=desk_id)
             if desk and desk.get('default_content_profile') and not doc.get('macro'):
-                dest_doc['profile'] = desk['default_content_profile']
+                dest_doc.setdefault('profile', desk['default_content_profile'])
 
             if dest_doc.get('type', 'text') in MEDIA_TYPES:
                 dest_doc['profile'] = None
