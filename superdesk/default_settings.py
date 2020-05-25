8de2ac7c66516e3422fe67dd66b6e94ec694ade2
Minh Vo <45254285+idsvn@users.noreply.github.com>
2020-05-25 20:28:44 +0700

    Video editor (#1668)
    * Merge superdesk branch 'github/master'
    
    * Add video editor service for integrating with Superdesk.
    
    * Add setting for video-server
    
    * Upload file video to video-server
    
    * update upload file function avoid duplicate.
    
    * Catch connection error for video editor and update changes follow new update in video server.
    
    * Add video edit service
    
    * Catch connection error for video editor and update changes follow new update in video server.
    
    * Fix use GUID instead of Object for video item
    
    * Add upload preview thumbnail
    
    * Update setup.py
    
    update lib hachoir<=3.0a3
    
    * add capture thumbnail when edit video.
    
    * update the upload thumbnail
    
    * convert negative to positive of crop value
    
    * Move str into _url method, and rename VIDEO_SERVER_HOST to VIDEO_SERVER_URL.
    
    * remove un-use code and rename media to video_editor_id.
    
    * fix update data for video edit
    
    * Update default for preview thumbnail's mimetype
    
    * update docstrings
    
    * update the videoEditorWrapper, and update validate for edit and capture.
    
    * update name pep8 format
    
    * update some stuffs
    
    * Fix incorrect crop param type in capture preview thumbnail
    
    Change from dict to string format: x,y,width,height
    
    * Add missing position param in capture thumbnail
    
    * init value for response
    
    * Remove unused import
    
    * Add test for get, capture timeline, upload thumbnail video
    
    * add test case for upload, edit, capture video
    
    * Avoid changing 'etag' during patching archive item from 'VideoEditService'.
    
    * Remove json dump for error response from video editor
    
    * Update schema for video edit item, remove unused item methods
    
    * Rename video edit test
    
    * Use video_editor_id in renditions instead of media
    
    * Add validation for missing both capture and edit param
    
    It's much simpler to add validation logic in function than
    using cerberus for oneof rule with capture and edit
    With Cerberus empty is False rule, capture and edit should not
    be empty, thus it's unnecessary to check if it's falsy
    
    * Fix projection issue
    
    * Adjust settings and validation messages.
    
    Co-authored-by: Thanh Nguyen <thanh.nguyentan@idsolutions.com.vn>
    Co-authored-by: Loi Tran <loi.tran@idsolutions.com.vn>
    Co-authored-by: ride90 <olegpshenichniy@gmail.com>

--- superdesk/default_settings.py
+++ superdesk/default_settings.py
@@ -346,6 +346,7 @@ CORE_APPS = [
     'superdesk.allowed_values',
     'apps.picture_crop',
     'apps.picture_renditions',
+    'apps.video_edit',
     'content_api.publish',
     'content_api.items',
     'content_api.tokens',
@@ -782,7 +783,7 @@ KILL_TEMPLATE_NULL_FIELDS = ['byline', 'place']
 
 #: Video editor server
 VIDEO_SERVER_URL = env('VIDEO_SERVER_URL', 'http://localhost:5050')
-VIDEO_SERVER_ENABLE = strtobool(env('VIDEO_SERVER_ENABLE', 'false'))
+VIDEO_SERVER_ENABLED = strtobool(env('VIDEO_SERVER_ENABLED', 'false'))
 
 #: Whether to generate custom crops on image upload by default or not, if false means generate else don't generate
 NO_CUSTOM_CROPS = strtobool(env('NO_CUSTOM_CROPS', 'false'))
