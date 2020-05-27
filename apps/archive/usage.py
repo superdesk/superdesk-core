b94bfcd7cdf54ca04dd29576c1b69d5474314bf7
Petr Jasek <jasekpetr@gmail.com>
2020-05-27 09:59:28 +0200

    update usage status on published items
    SDANSA-395

--- apps/archive/usage.py
+++ apps/archive/usage.py
@@ -1,7 +1,13 @@
 
+import bson
+import logging
 import superdesk
 
 from superdesk.utc import utcnow
+from superdesk.metadata.item import ITEM_STATE, PUBLISH_STATES
+
+
+logger = logging.getLogger(__name__)
 
 
 def track_usage(media_item, stored_item, item_obj, item_name, original):
@@ -26,6 +32,16 @@ def _update_usage(item):
     }
 
     superdesk.get_resource_service('archive').system_update(item['_id'], updates, item)
+
+    # update published item state as well
+    if item.get(ITEM_STATE) in PUBLISH_STATES:
+        published = superdesk.get_resource_service('published').get_last_published_version(item['_id'])
+        if published:
+            superdesk.get_resource_service('published').system_update(bson.ObjectId(published['_id']),
+                                                                      updates, published)
+        else:
+            logger.warning('published item not found for item %s', item['_id'])
+
     item.update(updates)
 
 
