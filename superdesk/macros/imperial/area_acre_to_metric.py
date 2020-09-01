1df06a039127afad6c5773d23ce09da74a25d64c
GyanP <gyanig72@gmail.com>
2020-09-01 11:29:47 +0530

    Minor change.

--- superdesk/macros/imperial/area_acre_to_metric.py
+++ superdesk/macros/imperial/area_acre_to_metric.py
@@ -9,6 +9,7 @@
 # at https://www.sourcefabric.org/superdesk/license
 from decimal import Decimal
 from . import unit_base
+from flask_babel import _
 
 SQUARE_METER_SYMBOL = 'square meter'
 HECTARE_SYMBOL = 'ha'
