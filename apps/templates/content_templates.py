b5c7a67e0542fe07873f33678874dd7f0cf8a867
GyanP <gyanig72@gmail.com>
2020-10-29 18:29:55 +0530

    split the manage and not manage part

--- apps/templates/content_templates.py
+++ apps/templates/content_templates.py
@@ -197,16 +197,16 @@ class ContentTemplatesService(BaseService):
         privileges = active_user.get('active_privileges', {})
         if not lookup:
             lookup = {}
-        manage = req.args.get('manage')
-
-        if manage and not privileges.get('content_templates') and not privileges.get('personal_template'):
-            lookup.update({'$and': [{'is_public': False}, {'user': active_user.get('_id')}]})
-        elif manage and not privileges.get('content_templates') and privileges.get('personal_template'):
-            lookup.update({'$or': [{'is_public': False}, {'user': active_user.get('_id')}]})
-        elif (manage and privileges.get('content_templates')
-                and not privileges.get('personal_template')
-                or not manage and not privileges.get('personal_template')):
-            lookup.update({'$or': [{'is_public': True}, {'user': active_user.get('_id')}]})
+        if req and req.args and req.args.get('manage'):
+            if not privileges.get('content_templates') and not privileges.get('personal_template'):
+                lookup.update({'$and': [{'is_public': False}, {'user': active_user.get('_id')}]})
+            elif not privileges.get('content_templates') and privileges.get('personal_template'):
+                lookup.update({'$or': [{'is_public': False}, {'user': active_user.get('_id')}]})
+            elif (privileges.get('content_templates') and not privileges.get('personal_template')):
+                lookup.update({'$or': [{'is_public': True}, {'user': active_user.get('_id')}]})
+        else:
+            if not privileges.get('personal_template'):
+                lookup.update({'$or': [{'is_public': True}, {'user': active_user.get('_id')}]})
         results = super().get(req, lookup)
 
         return results
