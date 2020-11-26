473d29451add62a713b12ceb5688fb0994c4a82b
Petr Jasek <jasekpetr@gmail.com>
2020-11-26 15:08:19 +0100

    fix firstpublished according to scheduled
    scheduled utc is only populated after `super().on_update` runs,
    clients send it using timezone.
    
    SDESK-5677

--- apps/publish/content/publish.py
+++ apps/publish/content/publish.py
@@ -42,6 +42,9 @@ class ArchivePublishService(BasePublishService):
                 raise SuperdeskApiError.badRequestError(_("Empty package cannot be published!"))
 
     def on_update(self, updates, original):
+        updates[ITEM_OPERATION] = self.item_operation
+        super().on_update(updates, original)
+
         if not original.get('firstpublished'):
             if updates.get(SCHEDULE_SETTINGS) and updates[SCHEDULE_SETTINGS].get('utc_publish_schedule'):
                 updates['firstpublished'] = updates[SCHEDULE_SETTINGS]['utc_publish_schedule']
@@ -53,8 +56,6 @@ class ArchivePublishService(BasePublishService):
             updates['previous_marked_user'] = original['marked_for_user']
             updates['marked_for_user'] = None
 
-        updates[ITEM_OPERATION] = self.item_operation
-        super().on_update(updates, original)
         set_sign_off(updates, original)
         update_word_count(updates)
 
