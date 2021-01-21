de06bd50edd277898070deb0d99126142062e67c
GyanP <gyanig72@gmail.com>
2021-01-21 11:54:37 +0530

    As a system admin I want to display a system-wide message to all logged in users. [SDESK-5659]

---
+++ apps/system_message/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8; -*-
+#
+# This file is part of Superdesk.
+#
+# Copyright 2013, 2014 Sourcefabric z.u. and contributors.
+#
+# For the full copyright and license information, please see the
+# AUTHORS and LICENSE files distributed with this source code, or
+# at https://www.sourcefabric.org/superdesk/license
+
+import logging
+import superdesk
+from .service import SystemMessagesService
+from .resource import SystemMessagesResource
+
+logger = logging.getLogger(__name__)
+
+
+def init_app(app):
+    endpoint_name = "system_message"
+    service = SystemMessagesService(endpoint_name, backend=superdesk.get_backend())
+    SystemMessagesResource(endpoint_name, app=app, service=service)
