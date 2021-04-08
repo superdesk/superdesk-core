3eadca704fceb3fa2971922dcd24db79ee05d6a8
Petr Jasek <jasekpetr@gmail.com>
2021-04-08 15:51:19 +0200

    fix content api keeping old renditions
    in case when new picture does not provide some
    due to its size.
    
    SDESK-5877

--- content_api/items/service.py
+++ content_api/items/service.py
@@ -263,12 +263,15 @@ class ItemsService(BaseService):
     def _process_item_renditions(self, item):
         hrefs = {}
         if item.get("renditions"):
-            for _k, v in item["renditions"].items():
-                if "media" in v:
+            renditions = {}
+            for k, v in item["renditions"].items():
+                if v and "media" in v:
                     href = v.get("href")
                     media = v.pop("media")
                     v["href"] = app.media.url_for_media(media, v.get("mimetype"))
                     hrefs[href] = v["href"]
+                    renditions[k] = v
+            item["renditions"] = renditions  # filter out empty renditions
         return hrefs
 
     def _process_item_associations(self, item):
