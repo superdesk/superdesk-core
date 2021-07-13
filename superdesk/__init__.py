b515b424e9001d437a1f00cdd1076f8717378864
Petr Jasek <jasekpetr@gmail.com>
2021-07-13 07:09:49 +0200

    bump version

--- superdesk/__init__.py
+++ superdesk/__init__.py
@@ -31,7 +31,7 @@ from .signals import *  # noqa
 from apps.common.models.base_model import BaseModel
 from apps.common.components.base_component import BaseComponent
 
-__version__ = "2.3.0"
+__version__ = "2.4.0.dev0"
 
 API_NAME = "Superdesk API"
 SCHEMA_VERSION = 2
