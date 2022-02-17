71f43b74e4ccb0e99025746fc2b9e1b08dc54dd3
Mark Pittaway <mark.pittaway@gmail.com>
2022-02-18 09:55:24 +1100

    fix: lint issues

--- apps/auth/auth.py
+++ apps/auth/auth.py
@@ -156,7 +156,10 @@ class SuperdeskTokenAuth(TokenAuth):
             if method in ("POST", "PUT", "PATCH") or method == "GET" and not request.args.get("auto"):
                 now = utcnow()
                 auth_updated = False
-                if auth_token[app.config["LAST_UPDATED"]] + timedelta(seconds=app.config["SESSION_UPDATE_SECONDS"]) < now:
+                if (
+                    auth_token[app.config["LAST_UPDATED"]] + timedelta(seconds=app.config["SESSION_UPDATE_SECONDS"])
+                    < now
+                ):
                     auth_service.update_session({app.config["LAST_UPDATED"]: now})
                     auth_updated = True
                 if not flask.g.user.get("last_activity_at") or auth_updated:
