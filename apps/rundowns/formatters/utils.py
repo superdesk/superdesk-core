0e67e13c743d34866aaa98276225cb845598ff97
Petr Jasek <jasekpetr@gmail.com>
2022-10-11 14:11:26 +0200

    generate last sentence in realizer output

--- apps/rundowns/formatters/utils.py
+++ apps/rundowns/formatters/utils.py
@@ -1,6 +1,9 @@
 from typing import List
 from datetime import datetime, timedelta
 
+from superdesk.text_utils import get_text
+from superdesk.editor_utils import get_field_content_state
+
 from .. import types
 
 
@@ -15,6 +18,26 @@ def item_title(show: types.IShow, rundown: types.IRundown, item: types.IRundownI
             ],
         )
     )
+
+
+def format_last_sentence(item) -> str:
+    text = ""
+    content_state = get_field_content_state(item, "content")
+    if content_state:
+        for block in content_state["blocks"]:
+            if block.get("text"):
+                text = block.get("text")
+    elif item.get("content"):
+        text = get_text(item["content"], "html", lf_on_block=True).strip()
+    words = []
+    for line in text.splitlines():
+        for word in line.split():
+            words.append(word)
+    for i in range(-5, -1):
+        output = " ".join(words[i:])
+        if len(output) < 30:
+            return output
+    return ""
 
 
 def format_duration(duration) -> str:
@@ -32,6 +55,6 @@ def item_table_data(show: types.IShow, rundown: types.IRundown, item: types.IRun
         "Tone" if item.get("live_sound") else "OFF",
         item.get("additional_notes") or "",
         item.get("live_captions") or "",
-        item.get("last_sentence") or "",
+        format_last_sentence(item),
         format_duration(item.get("duration")),
     ]
