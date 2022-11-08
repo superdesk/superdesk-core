0a5f5e9fffb9353ea40a0c95ed2d53b0204a2ee9
Mark Pittaway <mark.pittaway@gmail.com>
2022-11-08 17:07:01 +1100

    add tests

--- tests/websockets_test.py
+++ tests/websockets_test.py
@@ -1,14 +1,17 @@
+from typing import List
 import asyncio
 import unittest
 from json import dumps
 from datetime import datetime, timedelta
 from superdesk.websockets_comms import SocketCommunication
+from superdesk.types import WebsocketMessageData
 
 
 class TestClient:
-    def __init__(self):
+    def __init__(self, path: str):
         self.open = True
-        self.messages = []
+        self.messages: List[str] = []
+        self.path = path
 
     def send(self, message):
         self.messages.append(message)
@@ -18,7 +21,7 @@ class WebsocketsTestCase(unittest.TestCase):
     def test_broadcast(self):
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
-        client = TestClient()
+        client = TestClient("")
         com = SocketCommunication("host", "port", "url")
         com.clients.add(client)
 
@@ -44,3 +47,92 @@ class WebsocketsTestCase(unittest.TestCase):
             )
         )
         self.assertEqual(4, len(client.messages))
+
+    def test_broadcast_specific_recipients(self):
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        com = SocketCommunication("host", "port", "url")
+
+        client_no_user = TestClient("")
+        client_user_abc123_sess_12345 = TestClient("/subscribe?user=abc123&session=12345")
+        client_user_abc123_sess_67890 = TestClient("/subscribe?user=abc123&session=67890")
+        client_user_def456 = TestClient("/subscribe?user=def456")
+        com.clients.add(client_no_user)
+        com.clients.add(client_user_abc123_sess_12345)
+        com.clients.add(client_user_abc123_sess_67890)
+        com.clients.add(client_user_def456)
+
+        # 1. Send to all consumers
+        loop.run_until_complete(com.broadcast(dumps({"event": "new_items", "_created": datetime.now().isoformat()})))
+        self.assertEqual(1, len(client_no_user.messages))
+        self.assertEqual(1, len(client_user_abc123_sess_12345.messages))
+        self.assertEqual(1, len(client_user_abc123_sess_67890.messages))
+        self.assertEqual(1, len(client_user_def456.messages))
+
+        # Send to user: abc123
+        loop.run_until_complete(
+            com.broadcast(
+                dumps(
+                    WebsocketMessageData(
+                        event="new_items",
+                        _created=datetime.now().isoformat(),
+                        filters=dict(include={"user": ["abc123"]}),
+                    ),
+                )
+            )
+        )
+        self.assertEqual(1, len(client_no_user.messages))
+        self.assertEqual(2, len(client_user_abc123_sess_12345.messages))
+        self.assertEqual(2, len(client_user_abc123_sess_67890.messages))
+        self.assertEqual(1, len(client_user_def456.messages))
+
+        # Send to users: abc123 & def456
+        loop.run_until_complete(
+            com.broadcast(
+                dumps(
+                    WebsocketMessageData(
+                        event="new_items",
+                        _created=datetime.now().isoformat(),
+                        filters=dict(include={"user": ["abc123", "def456"]}),
+                    ),
+                )
+            )
+        )
+        self.assertEqual(1, len(client_no_user.messages))
+        self.assertEqual(3, len(client_user_abc123_sess_12345.messages))
+        self.assertEqual(3, len(client_user_abc123_sess_67890.messages))
+        self.assertEqual(2, len(client_user_def456.messages))
+
+        # Send to user: abc123, session: 67890
+        loop.run_until_complete(
+            com.broadcast(
+                dumps(
+                    WebsocketMessageData(
+                        event="new_items",
+                        _created=datetime.now().isoformat(),
+                        filters=dict(include={"user": ["abc123"], "session": ["67890"]}),
+                    ),
+                )
+            )
+        )
+        self.assertEqual(1, len(client_no_user.messages))
+        self.assertEqual(3, len(client_user_abc123_sess_12345.messages))
+        self.assertEqual(4, len(client_user_abc123_sess_67890.messages))
+        self.assertEqual(2, len(client_user_def456.messages))
+
+        # Send to all consumers except user: abc123
+        loop.run_until_complete(
+            com.broadcast(
+                dumps(
+                    WebsocketMessageData(
+                        event="new_items",
+                        _created=datetime.now().isoformat(),
+                        filters=dict(exclude={"user": ["abc123"]}),
+                    ),
+                )
+            )
+        )
+        self.assertEqual(2, len(client_no_user.messages))
+        self.assertEqual(3, len(client_user_abc123_sess_12345.messages))
+        self.assertEqual(4, len(client_user_abc123_sess_67890.messages))
+        self.assertEqual(3, len(client_user_def456.messages))
