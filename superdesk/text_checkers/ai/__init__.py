708d137677190fbab870e29a43e944805b3251e5
Petr Jašek <jasekpetr@gmail.com>
2022-12-01 12:40:02 +0100

    send current tags when analyzing article via imatrics (#2405)
    * send current tags when analyzing article via imatrics
    
    SDNTB-785
    
    * run tests on python 3.8

--- superdesk/text_checkers/ai/__init__.py
+++ superdesk/text_checkers/ai/__init__.py
@@ -38,6 +38,10 @@ class AIResource(Resource):
                 "body_html": {"type": "string", "required": True},
             },
         },
+        "tags": {
+            "type": "dict",
+            "required": False,
+        },
     }
     datasource = {"projection": {"analysis": 1}}
     internal_resource = False
@@ -82,7 +86,7 @@ class AIService(BaseService):
         except KeyError:
             raise SuperdeskApiError.notFoundError("{service} service can't be found".format(service=service))
 
-        analyzed_data = service.analyze(item)
+        analyzed_data = service.analyze(item, doc.get("tags"))
         docs[0].update({"analysis": analyzed_data})
         return [0]
 
