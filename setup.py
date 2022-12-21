1cffd19cd1f89388d6c9d9f861001dcd92cbbae1
Petr Jasek <jasekpetr@gmail.com>
2022-12-21 15:16:52 +0100

    loosen eve-elastic version requirements
    so that we can bump its version without changing core

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ install_requires = [
     # and https://github.com/psf/requests/pull/5651)
     "urllib3<1.26",
     "eve==1.1.2",
-    "eve-elastic==7.3.0",
+    "eve-elastic>=7.3.0,<7.4.0",
     "flask>=1.1,<1.2",
     "flask-mail>=0.9,<0.10",
     "flask-script>=2.0.5,<3.0",
