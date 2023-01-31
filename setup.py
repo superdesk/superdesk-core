4227cbda7b137b5340ce0a4a495e4a9bf2358b3c
Petr Jašek <jasekpetr@gmail.com>
2023-01-31 07:36:54 +0100

    allow newer elastic-apm agent (#2424)

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ install_requires = [
     "flask-oidc-ex==0.5.5",
     # to be replaced by stdlib version when we use Python 3.8+
     "typing_extensions>=3.7.4",
-    "elastic-apm[flask]>=6.7,<6.8",
+    "elastic-apm[flask]>=6.7,<7",
     # Fix an issue with MarkupSafe 2.1.0 not exporting `soft_unicode`
     "MarkupSafe<2.1",
 ]
