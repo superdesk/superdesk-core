3de0597235beab5e36e5238715cb8c7566c374a5
tcp-bhargav <115644367+tcp-bhargav@users.noreply.github.com>
2023-08-16 15:12:16 -0400

    Update imatrics.py
    V2: Added Logging

--- superdesk/publish/transmitters/imatrics.py
+++ superdesk/publish/transmitters/imatrics.py
@@ -3,12 +3,19 @@ from flask import current_app, json
 from superdesk.publish import register_transmitter
 from superdesk.publish.publish_service import PublishService
 from superdesk.text_checkers.ai.imatrics import IMatrics
+import logging
 
+logging.basicConfig(level=logging.DEBUG) 
+logger = logging.getLogger(__name__)
 
 class IMatricsTransmitter(PublishService):
     def _transmit(self, queue_item, subscriber):
         imatrics = IMatrics(current_app)
         item = json.loads(queue_item["formatted_item"])
+
+        logger.warning("Logging in Transmitter for IMAtrics")
+        logging.debug("Transmitting item: %s", item)
+        
         imatrics.publish(item)
 
 
