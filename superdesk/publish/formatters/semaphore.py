ad38e5c87239e8871fb9449287f870b314c4cedd
tcp-bhargav <115644367+tcp-bhargav@users.noreply.github.com>
2023-09-20 14:21:44 -0400

    Create semaphore.py
    Created Semaphore.py Formatter file

---
+++ superdesk/publish/formatters/semaphore.py
@@ -0,0 +1,26 @@
+import logging
+from superdesk.text_utils import get_text
+from .ninjs_formatter import NINJSFormatter
+from superdesk.text_checkers.ai.semaphore import Semaphore  # Import the Semaphore integration class
+
+logger = logging.getLogger(__name__)
+
+class SemaphoreFormatter(NINJSFormatter):
+    def can_format(self, format_type, article):
+        return format_type.lower() == "semaphore" and article.get("type") == "text"
+
+    def _transform_to_ninjs(self, article, subscriber, recursive=True):
+        semaphore = Semaphore()  # Initialize the Semaphore integration
+        formatted_data = {}  # Define how you want to format the data for Semaphore
+
+        try:
+            # Example: format the data
+            formatted_data["uuid"] = article["guid"]
+            formatted_data["headline"] = get_text(article["headline"])
+            # Add more formatting logic here
+
+        except Exception as e:
+            logger.error(f"Error formatting data for Semaphore: {str(e)}")
+            formatted_data = {}  # Return an empty dictionary in case of an error
+
+        return formatted_data
