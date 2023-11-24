0989b172d09aa59750bd72dbe45537142f3d3dff
devketanpro <devketanpro11@gmail.com>
2023-11-24 18:13:14 +0530

    add a new migration script to update width for existing profile fields

---
+++ superdesk/commands/update_width_for_profiles.py
@@ -0,0 +1,33 @@
+import superdesk
+import bson
+from copy import deepcopy
+
+
+class UpdateWidthForProfiles(superdesk.Command):
+    """
+    Update width for fields in content profiles
+    Example:
+    ::
+
+        $ manage.py app:updateWidthForProfiles
+
+    """
+
+    def run(self):
+        """
+        Run the command to update width for fields in content profiles.
+        """
+        content_profiles_service = superdesk.get_resource_service("content_types")
+        for profile in content_profiles_service.get(req=None, lookup=None):
+            try:
+                editor = profile.get("editor", {})
+                for field, properties in editor.items():
+                    if properties and "sdWidth" not in properties:
+                        properties["sdWidth"] = "full"
+                content_profiles_service.system_update(bson.ObjectId(profile["_id"]), {"editor": editor}, profile)
+                print(f"Content Profile {profile['_id']} updated successfully")
+            except Exception as e:
+                print(f"Error updating Content Profile {profile['_id']}: {str(e)}")
+
+
+superdesk.command("app:updateWidthForProfiles", UpdateWidthForProfiles())
