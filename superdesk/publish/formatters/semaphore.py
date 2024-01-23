7aa421a0edf99ceea2b2b20d5bc4428fd83a9120
tcp-bhargav <115644367+tcp-bhargav@users.noreply.github.com>
2024-01-23 15:49:54 -0500

    Update semaphore.py in Formatters to work with ninjs_formatter_2
    Update semaphore.py in Formatters to work with ninjs_formatter_2

--- superdesk/publish/formatters/semaphore.py
+++ superdesk/publish/formatters/semaphore.py
@@ -1,11 +1,11 @@
 import logging
 from superdesk.text_utils import get_text
-from .ninjs_formatter import NINJSFormatter
+from .ninjs_formatter_2 import NINJSFormatter_2
 from superdesk.text_checkers.ai.semaphore import Semaphore  # Import the Semaphore integration class
 
 logger = logging.getLogger(__name__)
 
-class SemaphoreFormatter(NINJSFormatter):
+class SemaphoreFormatter(NINJSFormatter_2):
     def can_format(self, format_type, article):
         return format_type.lower() == "semaphore" and article.get("type") == "text"
 
