3865999d269f9c426adeafa19b8feb4a2e9f6998
Petr Jašek <jasekpetr@gmail.com>
2024-05-14 09:16:48 +0200

    fix merge conflict

--- setup.py
+++ setup.py
@@ -55,13 +55,8 @@ install_requires = [
     "unidecode>=0.04.21,<=1.3.8",
     "authlib>0.14,<0.15",
     "draftjs-exporter[lxml]<2.2",
-<<<<<<< dependabot/pip/regex-2024.4.28
     "regex>=2020.7.14,<=2024.4.28",
     "flask-oidc-ex==0.5.5",
-=======
-    "regex>=2020.7.14,<2020.8",
-    "flask-oidc-ex>=0.5.5,<0.6",
->>>>>>> develop
     # to be replaced by stdlib version when we use Python 3.8+
     "typing_extensions>=3.7.4",
     "elastic-apm[flask]>=6.15.1,<7.0",
