c3c37110888c344ca27dc2fa4701465b868bc558
Petr Jašek <jasekpetr@gmail.com>
2024-05-14 09:17:18 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ install_requires = [
     "authlib>0.14,<0.15",
     "draftjs-exporter[lxml]<2.2",
     "regex>=2020.7.14,<=2024.4.28",
-    "flask-oidc-ex==0.5.5",
+    "flask-oidc-ex>=0.5.5,<0.6",
     # to be replaced by stdlib version when we use Python 3.8+
     "typing_extensions>=3.7.4",
     "elastic-apm[flask]>=6.15.1,<7.0",
