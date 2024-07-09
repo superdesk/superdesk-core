eedccc212478aa1b6aea42f33d1014b8d1f77eb1
devketanpro <devketanpro11@gmail.com>
2024-07-09 14:45:53 +0530

    update var name

--- apps/archive/archive.py
+++ apps/archive/archive.py
@@ -1276,7 +1276,7 @@ class ArchiveService(BaseService, HighlightsSearchMixin):
         """
 
         # No notification sent if user is not enabled mark for user notification
-        filter_user_list = [
+        users_with_enabled_notifications = [
             user
             for user in user_list
             if superdesk.get_resource_service("preferences").mark_for_user_notification_is_enabled(
@@ -1297,7 +1297,13 @@ class ArchiveService(BaseService, HighlightsSearchMixin):
 
         if add_activity:
             notify_and_add_activity(
-                activity_name, msg, resource=resource, item=item, user_list=filter_user_list, link=link, **data
+                activity_name,
+                msg,
+                resource=resource,
+                item=item,
+                user_list=users_with_enabled_notifications,
+                link=link,
+                **data,
             )
         # send separate notification for markForUser extension
         push_notification(
