79a432e37017c7e43acaa7ae6d6d99af72af16c5
Petr Jasek <jasekpetr@gmail.com>
2024-07-29 14:30:01 +0200

    release 2.7.2

--- superdesk/__init__.py
+++ superdesk/__init__.py
@@ -32,7 +32,7 @@ from .signals import *  # noqa
 from apps.common.models.base_model import BaseModel
 from apps.common.components.base_component import BaseComponent
 
-__version__ = "2.7.1"
+__version__ = "2.7.2"
 
 API_NAME = "Superdesk API"
 SCHEMA_VERSION = 2
