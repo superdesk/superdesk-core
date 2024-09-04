efee0b252b35f60ffffeb756b80bce7577303da5
Helmy Giacoman <helmygb@gmail.com>
2024-09-04 09:51:31 +0200

    Removing not needed settings
    SDESK-7284

--- tests/test_instance/server/app.py
+++ tests/test_instance/server/app.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # -*- coding: utf-8; -*-
 #
 # This file is part of Superdesk.
@@ -6,29 +5,6 @@
 # Copyright 2013 to present Sourcefabric z.u. and contributors.
 #
 # For the full copyright and license information, please see the
-# AUTHORS and LICENSE files distributed with this source code, or
-# at https://www.sourcefabric.org/superdesk/license
+# AUTHORS and LICENSE files distributed with this source code
 
-import os
-import settings  # type: ignore
-
-from superdesk.factory import get_app
-
-
-def create_app(config=None):
-    """App factory.
-
-    :param config: configuration that can override config from `settings.py`
-    :return: a new SuperdeskEve app instance
-    """
-    if config is None:
-        config = {}
-
-    config["APP_ABSPATH"] = os.path.abspath(os.path.dirname(__file__))
-
-    for key in dir(settings):
-        if key.isupper():
-            config.setdefault(key, getattr(settings, key))
-
-    app = get_app(config)
-    return app
+from superdesk.factory import get_app as create_app  # noqa
--- tests/test_instance/server/manage.py
+++ tests/test_instance/server/manage.py
@@ -1,13 +1,11 @@
-#!/usr/bin/env python
 # -*- coding: utf-8; -*-
 #
 # This file is part of Superdesk.
 #
-# Copyright 2013, to present Sourcefabric z.u. and contributors.
+# Copyright 2013 to present Sourcefabric z.u. and contributors.
 #
 # For the full copyright and license information, please see the
-# AUTHORS and LICENSE files distributed with this source code, or
-# at https://www.sourcefabric.org/superdesk/license
+# AUTHORS and LICENSE files distributed with this source code
 
 """Superdesk Manager"""
 
