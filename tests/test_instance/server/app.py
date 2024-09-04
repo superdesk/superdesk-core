d3de447edeb5840b29091604872dc4416090d8bf
Helmy Giacoman <helmygb@gmail.com>
2024-09-04 10:20:13 +0200

    [SDESK-7284] Launch superdesk test instance on Github Actions (#2684)
    * Implement new async_cli module
    
    This module includes a custom Blueprint, AppGroup and a helper to easily create a command blueprint ready to register commands based on async function. The registered async commands will be run sync using `asgiref.sync.async_to_sync`
    
    SDESK-7284
    
    * Rework `app:initialize_data` command
    
    The command was converted from the old class based commands into a function one using `Click` and our new `cli.register_async_command` decorator in order to use our new async services in the synchronous context of Quart's command line interface
    
    SDESK-7284
    
    * Update docker-compose.yml
    
    * Fix type and docstrings
    
    * Rework `users:create` command
    
    Command converted from the old class based commands into a function one using `Click` and our new `cli.register_async_command` decorator.
    
    SDESK-7284
    
    * Rework `schema:migrate` command
    
    SDESK-7284
    
    * Update app_initialize.py
    
    * Rework `data:upgrade` and `data:downgrade` commands
    
    SDESK-7284
    
    * Remove not needed false return
    
    * Fix async commands to use proper app context
    
    Async commands fail when using app context as they are executed in different threads or outside of the normal request context lifecycle. In order to fix that, we set the current app instance into the AsyncAppGroup so later it can be passed optionally to the command at the moment of its execution.
    
    SDESK-7284
    
    * Fix `schema:migrate` command
    
    SDESK-7284
    
    * Small improvement to use of app context in async commands
    
    SDESK-7284
    
    * Implement basic Github Actions test instance
    
    SDESK-7284
    
    * Removing not needed settings
    
    SDESK-7284

---
+++ tests/test_instance/server/app.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8; -*-
+#
+# This file is part of Superdesk.
+#
+# Copyright 2013 to present Sourcefabric z.u. and contributors.
+#
+# For the full copyright and license information, please see the
+# AUTHORS and LICENSE files distributed with this source code
+
+from superdesk.factory import get_app as create_app  # noqa
