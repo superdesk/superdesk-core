b8cb0f9cda49befb77fd4445709d594a8caa6a7e
Mark Pittaway <mark.pittaway@gmail.com>
2024-10-14 11:01:13 +1100

    impove processing of intrinsic_auth_rule

--- superdesk/core/auth/rules.py
+++ superdesk/core/auth/rules.py
@@ -12,17 +12,13 @@ async def login_required_auth_rule(request: Request) -> None:
 
 
 async def endpoint_intrinsic_auth_rule(request: Request) -> Any | None:
-    intrinsic_auth = getattr(request.endpoint, "authorize", None)
-    if intrinsic_auth is not None:
-        response = await intrinsic_auth(request)
-        if response is not None:
-            return response
+    methods = ["authorize", f"authorize_{request.method.lower()}"]
+    for method_name in methods:
+        intrinsic_auth = getattr(request.endpoint, method_name, None)
 
-    method = request.method.lower()
-    intrinsic_auth = getattr(request.endpoint, f"authorize_{method}", None)
-    if intrinsic_auth:
-        response = await intrinsic_auth(request)
-        if response is not None:
-            return response
+        if intrinsic_auth:
+            response = await intrinsic_auth(request)
+            if response is not None:
+                return response
 
     return None
