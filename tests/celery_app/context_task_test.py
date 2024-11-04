3ab21882cc179f7c77947debf04605eebca4eb67
Helmy Giacoman <helmygb@gmail.com>
2024-11-04 09:59:17 +0100

    Support eager mode in async celery (#2745)
    SDESK-7371

--- tests/celery_app/context_task_test.py
+++ tests/celery_app/context_task_test.py
@@ -3,17 +3,19 @@ from unittest.mock import patch
 
 from superdesk.errors import SuperdeskError
 from superdesk.celery_app import HybridAppContextTask
-from superdesk.tests import AsyncFlaskTestCase, markers
+from superdesk.tests import AsyncFlaskTestCase
+
+# NOTE: all tasks below are in eager mode because of global
+# tests settings. See `update_config` function in tests.__init__.py
 
 
-@markers.requires_async_celery
 class TestHybridAppContextTask(AsyncFlaskTestCase):
     async def test_sync_task(self):
         @self.app.celery.task(base=HybridAppContextTask)
         def sync_task():
             return "sync result"
 
-        result = sync_task.apply_async().get()
+        result = await sync_task.apply_async()
         self.assertEqual(result, "sync result")
 
     async def test_async_task(self):
@@ -22,7 +24,7 @@ class TestHybridAppContextTask(AsyncFlaskTestCase):
             await asyncio.sleep(0.1)
             return "async result"
 
-        result = await async_task.apply_async().get()
+        result = await async_task.apply_async()
         self.assertEqual(result, "async result")
 
     async def test_sync_task_exception(self):
@@ -31,7 +33,7 @@ class TestHybridAppContextTask(AsyncFlaskTestCase):
             raise SuperdeskError("Test exception")
 
         with patch("superdesk.celery_app.context_task.logger") as mock_logger:
-            sync_task_exception.apply_async().get(propagate=True)
+            await sync_task_exception.apply_async()
             expected_exc = SuperdeskError("Test exception")
             expected_msg = f"Error handling task: {str(expected_exc)}"
             mock_logger.exception.assert_called_once_with(expected_msg)
@@ -42,7 +44,7 @@ class TestHybridAppContextTask(AsyncFlaskTestCase):
             raise SuperdeskError("Async exception")
 
         with patch("superdesk.celery_app.context_task.logger") as mock_logger:
-            await async_task_exception.apply_async().get()
+            await async_task_exception.apply_async()
 
             expected_exc = SuperdeskError("Async exception")
             expected_msg = f"Error handling task: {str(expected_exc)}"
