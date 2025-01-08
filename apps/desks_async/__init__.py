573bebc2fcce3f310d89e39dd19b05942262b90e
Brian Mwangi <mwangikabiru21@gmail.com>
2025-01-08 15:04:25 +0300

    Create desks module and register resource config

--- apps/desks_async/__init__.py
+++ apps/desks_async/__init__.py
@@ -1,3 +1,7 @@
+from superdesk.core.module import Module
 from .desks_async_service import DesksAsyncService
+from .module import desks_resource_config
 
 __all__ = ["DesksAsyncService"]
+
+module = Module(name="apps.desks", resources=[desks_resource_config])
