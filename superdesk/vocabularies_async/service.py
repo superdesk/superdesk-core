f321cc987642ec66a496f9f695244873dc882abf
Helmy Giacoman <helmygb@gmail.com>
2025-02-05 14:45:36 +0100

    Split `resource_endpoints_test.py` as it is getting too big
    SDESK-7484

--- superdesk/vocabularies_async/service.py
+++ superdesk/vocabularies_async/service.py
@@ -11,7 +11,7 @@
 
 import json
 import logging
-from typing import Any, Dict, List, Optional, cast
+from typing import Any, Dict, List, Optional
 from typing import List
 
 from eve.methods.common import serialize_value
