07c7b4e26b6119d72cce6499202be7693fdbaf06
Mark Pittaway <mark.pittaway@gmail.com>
2025-03-12 12:22:13 +1100

    Add async publish commands

--- superdesk/publish_async/commands.py
+++ superdesk/publish_async/commands.py
@@ -1,6 +1,13 @@
+from pprint import pprint
+from superdesk.core import get_config
+
+from superdesk.types import PublishRequest, PublishRequestResponse, ContentType
 from superdesk.commands import cli
 from superdesk.celery_app import celery
-from .controller.exchanges import get_exchange_factory
+from superdesk.default_settings import PublishChannelConfig
+from superdesk.profiling import ProfileManager
+
+from . import get_exchange_factory
 
 
 @cli.command("publish:transmit")
@@ -11,12 +18,82 @@ async def publish_pending_items():
     This asynchronous function fetches the exchange factory and initiates
     the processing of pending tasks. Useful for ensuring any delayed or
     queued items are published as required.
-
-    Returns:
-        None
     """
 
     await get_exchange_factory().process_pending_tasks()
+
+
+@cli.command("publish:enqueue")
+async def publish_scheduled_items():
+    """
+    Command to transmit and publish all scheduled items (if the schedule has elapsed).
+
+    This asynchronous function fetches the exchange factory and initiates
+    the processing of pending tasks. Useful for ensuring any delayed or
+    queued items are published as required.
+    """
+    await get_exchange_factory().send_scheduled_or_pending_content()
+
+
+@cli.command("publish:print_types")
+def print_publish_component_types():
+    factory = get_exchange_factory()
+    channels = get_config(list[PublishChannelConfig], "PUBLISH_CHANNELS")
+
+    item_types = [
+        ContentType.TEXT,
+        ContentType.PREFORMATTED,
+        ContentType.AUDIO,
+        ContentType.VIDEO,
+        ContentType.PICTURE,
+        ContentType.GRAPHIC,
+        ContentType.COMPOSITE,
+        ContentType.EVENT,
+        ContentType.PLANNING,
+        ContentType.FEATURED_PLANNING,
+    ]
+    operations = [
+        "publish",
+        "correct",
+        "kill",
+        "takedown",
+        "unpublish",
+        "being_corrected",
+        "resend",
+    ]
+
+    print("Channel Configs:")
+    pprint(channels)
+
+    print("\nFilter Classes:")
+    pprint(factory._filter_classes)
+
+    print("\nFormatter Classes:")
+    pprint(factory._formatter_classes)
+
+    print("\nRouter Classes:")
+    pprint(factory._router_classes)
+
+    print("\nExchange Classes:")
+    pprint(factory._exchange_classes)
+
+    print("\nSupported Publish Channels:")
+    for item_type in item_types:
+        print(f"{item_type}:")
+        for operation in operations:
+            exchange = factory.get_exchange(
+                PublishRequest(
+                    item={},
+                    item_id="",
+                    operation=operation,
+                    published_state="published",
+                    item_type=item_type,
+                    sender_type="api",
+                )
+            )
+            print(
+                f"    {operation}: Exchange(exchange={exchange.name}, filter={exchange._filter.name}, formatter={exchange._formatter.name}, router={exchange._router.name})"
+            )
 
 
 @celery.task(soft_time_limit=1800, expires=10)
@@ -26,12 +103,17 @@ async def transmit():
     time constraints using Celery. This task is set up to have a soft time
     limit and an expiration time, ensuring efficient task execution and
     preventing runaway processes.
-
-    Args:
-        None
-
-    Returns:
-        None
     """
 
     await get_exchange_factory().process_pending_tasks()
+
+
+async def publish_item(request: PublishRequest) -> PublishRequestResponse:
+    return await get_exchange_factory().send(request)
+
+
+@celery.task(soft_time_limit=600)
+async def enqueue_published():
+    """Pick new items from ``published`` collection and enqueue it."""
+    with ProfileManager("publish:enqueue"):
+        await get_exchange_factory().send_scheduled_or_pending_content()
