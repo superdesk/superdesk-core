67884b6ffc5f1fa5dd38e8ddd281103a0f993ded
Mark Pittaway <mark.pittaway@gmail.com>
2025-04-28 17:19:16 +1000

    Update content_api publish to use async where possible

--- content_api/tokens/auth.py
+++ content_api/tokens/auth.py
@@ -5,7 +5,8 @@ from superdesk.errors import SuperdeskApiError
 from superdesk.core.types.web import AuthRule, Request
 from superdesk.core.auth.user_auth import UserAuthProtocol
 from superdesk.core.auth.rules import endpoint_intrinsic_auth_rule
-from superdesk.publish.subscriber_token import SubscriberTokenService, SubscriberToken
+from superdesk.types import SubscriberToken
+from superdesk.publish_async.resources.subscriber_token import SubscriberTokenService
 
 
 # TODO-ASYNC: Needed to avoid the content_api items endpoint from crashing
