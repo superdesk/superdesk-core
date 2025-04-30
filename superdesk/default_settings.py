74bfb435ec9114a7bddce32b3114a08f8cba171e
Mark Pittaway <mark.pittaway@gmail.com>
2025-04-30 10:44:29 +1000

    re-enable Archive Pydantic resources (required by global search)

--- superdesk/default_settings.py
+++ superdesk/default_settings.py
@@ -433,8 +433,8 @@ MODULES = [
     "apps.desks_async",
     "superdesk.vocabularies_async",
     # TODO-ASYNC: Disable these 2 for now
-    # "superdesk.archive_async",
-    # "superdesk.archived_async",
+    "superdesk.archive_async",
+    "superdesk.archived_async",
     "superdesk.content_types_async",
     "superdesk.publish_async.module",
     "superdesk.auth_server.oauth2",
