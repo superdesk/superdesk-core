f0abc07b13c73a21f9a80da51947c8c3ba9021aa
Petr Jasek <jasekpetr@gmail.com>
2025-05-06 10:15:12 +0200

    add privilege

--- apps/user_availability/__init__.py
+++ apps/user_availability/__init__.py
@@ -1,6 +1,8 @@
+import superdesk
+
+from flask_babel import lazy_gettext
 from celery.schedules import crontab
 from superdesk.celery_app import celery
-
 
 from .availability import AvailabilityResource, availability_service
 from .default_availability import DefaultAvailabilityResource, default_service
@@ -18,6 +20,12 @@ def init_app(app):
         "task": "apps.user_availability.generate_user_availability",
         "schedule": crontab(hour="1", minute="40"),
     }
+
+    superdesk.privilege(
+        name="user_availability",
+        label=lazy_gettext("View user availability"),
+        description=lazy_gettext("User can view user availability overview"),
+    )
 
 
 @celery.task
