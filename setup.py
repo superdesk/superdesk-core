e7d71cc426fae57ac5e808ee4a6e9abf57b06df9
Petr Jasek <jasekpetr@gmail.com>
2025-07-07 08:40:07 +0200

    bump version

--- setup.py
+++ setup.py
@@ -87,7 +87,7 @@ package_data = {
 
 setup(
     name="Superdesk-Core",
-    version="2.9.0",
+    version="2.10.0.dev1",
     description="Superdesk Core library",
     long_description=LONG_DESCRIPTION,
     author="petr jasek",
