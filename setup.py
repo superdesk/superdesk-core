520797bedf1c6aae989e7f1fce07f1a972e29a3d
Mark Pittaway <mark.pittaway@gmail.com>
2025-09-01 13:52:54 +1000

    Release 3.0.0.beta.1

--- setup.py
+++ setup.py
@@ -90,7 +90,7 @@ package_data = {
 
 setup(
     name="Superdesk-Core",
-    version="3.0.0.dev0",
+    version="3.0.0.beta.1",
     description="Superdesk Core library",
     long_description=LONG_DESCRIPTION,
     author="petr jasek",
