d23dbd811abc9110c5ab24df2c95b3343f88ef35
Mark Pittaway <mark.pittaway@gmail.com>
2025-10-03 16:28:52 +1000

    chore: Set version to 3.2.0-dev.0

--- superdesk/__init__.py
+++ superdesk/__init__.py
@@ -33,7 +33,7 @@ from .signals import *  # noqa
 from apps.common.models.base_model import BaseModel
 from apps.common.components.base_component import BaseComponent
 
-__version__ = "3.1.0-dev.0"
+__version__ = "3.2.0-dev.0"
 
 API_NAME = "Superdesk API"
 SCHEMA_VERSION = 2
