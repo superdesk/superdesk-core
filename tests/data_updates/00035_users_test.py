a5b08ff20ea951d25f026b8bc5c2fff264b9a7b6
Mark Pittaway <mark.pittaway@gmail.com>
2025-10-03 11:07:31 +1000

    fix data_update test

--- tests/data_updates/00035_users_test.py
+++ tests/data_updates/00035_users_test.py
@@ -1,21 +1,26 @@
 import importlib
 
+from superdesk.tests import TestCase
+
 update_module = importlib.import_module("superdesk.data_updates.00035_20250918-140342_users")
 
 
-def test_forwards(app):
-    app.data.insert(
-        "users",
-        [
-            {
-                "_id": "user1",
-                "user_preferences": {"monitoring:view": {"allowed": True, "category": "news", "other_key": "value"}},
-            }
-        ],
-    )
-    data_update = update_module.DataUpdate()
-    data_update.forwards(app.data.driver.db.users, app.data.driver.db)
-    updated = app.data.find_one("users", req=None, _id="user1")
-    assert "allowed" not in updated["user_preferences"]["monitoring:view"]
-    assert "category" not in updated["user_preferences"]["monitoring:view"]
-    assert updated["user_preferences"]["monitoring:view"].get("other_key") == "value"
+class DataUpdate00035UsersTestCase(TestCase):
+    async def test_forwards(self):
+        self.app.data.insert(
+            "users",
+            [
+                {
+                    "_id": "user1",
+                    "user_preferences": {
+                        "monitoring:view": {"allowed": True, "category": "news", "other_key": "value"}
+                    },
+                }
+            ],
+        )
+        data_update = update_module.DataUpdate()
+        data_update.forwards(self.app.data.driver.db.users, self.app.data.driver.db)
+        updated = self.app.data.find_one("users", req=None, _id="user1")
+        assert "allowed" not in updated["user_preferences"]["monitoring:view"]
+        assert "category" not in updated["user_preferences"]["monitoring:view"]
+        assert updated["user_preferences"]["monitoring:view"].get("other_key") == "value"
