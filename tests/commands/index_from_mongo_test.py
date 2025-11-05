5571af99ce1688f8a65524a13d638f6f7564b4f7
Petr Jašek <jasekpetr@gmail.com>
2025-11-05 09:07:21 +0100

    add date param to index from mongo (#3059)
    SDESK-7791

---
+++ tests/commands/index_from_mongo_test.py
@@ -0,0 +1,45 @@
+from datetime import datetime
+from superdesk.tests import TestCase
+from superdesk.commands.index_from_mongo import IndexFromMongo
+
+
+class IndexFromMongoTestCase(TestCase):
+    def test_index_from_mongo_with_date(self):
+        self.app.data.pymongo("archive").db["archive"].insert_many(
+            [
+                {"_id": "1", "headline": "Old Archive", "_updated": datetime(2025, 1, 1, 5)},
+                {"_id": "2", "headline": "New Archive", "_updated": datetime(2025, 1, 1, 10)},
+            ]
+        )
+
+        def query_elastic():
+            return self.app.data.elastic.search({"query": {"match_all": {}}}, "archive")
+
+        resp = query_elastic()
+        assert resp.count() == 0
+
+        kwargs = {
+            "collection_name": "archive",
+            "all_collections": False,
+            "page_size": None,
+            "last_id": None,
+            "string_id": None,
+        }
+
+        IndexFromMongo().run(**kwargs, date="2025-01-02")
+
+        resp = query_elastic()
+        assert resp.count() == 0
+
+        IndexFromMongo().run(**kwargs, date="2025-01-01T08:00")
+
+        resp = query_elastic()
+        assert resp.count() == 1
+
+        IndexFromMongo().run(**kwargs, date="2025-01-01")
+
+        resp = query_elastic()
+        assert resp.count() == 2
+
+        with self.assertRaises(ValueError):
+            IndexFromMongo().run(**kwargs, date="2025")
