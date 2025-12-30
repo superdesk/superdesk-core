2e584b7303ca728ad4d0016b429a6dfc50404053
Petr Jasek <jasekpetr@gmail.com>
2025-12-30 13:47:46 +0100

    Revert "Apply suggestions from code review"
    This reverts commit 83331bf2568a9bba2b8476c5117cd983de60bd0e.

--- tests/publish_async/subscribers_security_test.py
+++ tests/publish_async/subscribers_security_test.py
@@ -1,13 +1,12 @@
 import json
 from superdesk.tests import TestCase, setup_db_user
 from superdesk.publish_async.resources.subscribers.service import SubscribersService
-from superdesk import get_resource_service
 
 
 class SubscribersSecurityTestCase(TestCase):
     def setUp(self):
         self.endpoint = "subscribers"
-        self.service = get_resource_service("subscribers")
+        self.service = SubscribersService()
         self.headers = [("Content-Type", "application/json")]
 
     async def test_nosql_injection_password_regex(self):
