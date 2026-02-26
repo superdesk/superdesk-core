b235511beab3c66ad85534d989bec3249278b453
Petr Jasek <jasekpetr@gmail.com>
2026-02-26 12:43:33 +0100

    release v2.11.1

--- setup.py
+++ setup.py
@@ -87,7 +87,7 @@ package_data = {
 
 setup(
     name="Superdesk-Core",
-    version="2.11.0",
+    version="2.11.1",
     description="Superdesk Core library",
     long_description=LONG_DESCRIPTION,
     author="petr jasek",
