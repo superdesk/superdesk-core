2f1612a2a40904e0d0f395289534c404234ca85c
Petr Jasek <jasekpetr@gmail.com>
2026-02-26 11:42:29 +0100

    make test stable

--- tests/storage/amazon_media_storage_test.py
+++ tests/storage/amazon_media_storage_test.py
@@ -54,13 +54,15 @@ class AmazonMediaStorageTestCase(TestCase):
             media_id = self.amazon.media_id(filename)
             self.assertEqual(filename, media_id)
 
-        with patch.dict(self.app.config, {"AMAZON_MEDIA_ID_TIME_PREFIX": "daily"}):
-            media_id = self.amazon.media_id(filename)
-            self.assertEqual("%s/%s" % (time.strftime("%Y%m%d"), filename), media_id)
+        with patch("superdesk.storage.amazon_media_storage.time.strftime", return_value="20260102"):
+            with patch.dict(self.app.config, {"AMAZON_MEDIA_ID_TIME_PREFIX": "daily"}):
+                media_id = self.amazon.media_id(filename)
+                self.assertEqual("20260102/%s" % filename, media_id)
 
-        with patch.dict(self.app.config, {"AMAZON_MEDIA_ID_TIME_PREFIX": "hourly"}):
-            media_id = self.amazon.media_id(filename)
-            self.assertEqual("%s/%s" % (time.strftime("%Y%m%d%H"), filename), media_id)
+        with patch("superdesk.storage.amazon_media_storage.time.strftime", return_value="2026010215"):
+            with patch.dict(self.app.config, {"AMAZON_MEDIA_ID_TIME_PREFIX": "hourly"}):
+                media_id = self.amazon.media_id(filename)
+                self.assertEqual("2026010215/%s" % filename, media_id)
 
     def test_put_and_delete(self):
         """Test amazon if configured.
