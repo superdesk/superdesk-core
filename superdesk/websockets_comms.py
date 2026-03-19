99ad67618dac1f98befd85bf3d36af190599a821
Mark Pittaway <mark.pittaway@gmail.com>
2026-03-20 09:02:44 +1100

    fix(args): Convert env vars to int/float

--- superdesk/websockets_comms.py
+++ superdesk/websockets_comms.py
@@ -81,20 +81,20 @@ class SocketBrokerClient:
             self.url,
             heartbeat=WS_HEART_BEAT,
             transport_options={
-                "socket_connect_timeout": env("WS_REDIS_CONNECT_TIMEOUT", 2),
+                "socket_connect_timeout": float(env("WS_REDIS_CONNECT_TIMEOUT", 2)),
                 # If no message has been consumed in 10 minutes, allow health check to run
                 # otherwise consumer loop hangs indefinitely on read timeout
-                "socket_timeout": env("WS_REDIS_TIMEOUT", 600),
+                "socket_timeout": float(env("WS_REDIS_TIMEOUT", 600)),
                 "retry_on_timeout": True,
                 # Enable TCP keepalive
                 "socket_keepalive": True,
                 "socket_keepalive_options": {
                     # Start probing after 30s idle
-                    socket.TCP_KEEPIDLE: env("WS_REDIS_KEEPALIVE_IDLE", 30),
+                    socket.TCP_KEEPIDLE: int(env("WS_REDIS_KEEPALIVE_IDLE", 30)),
                     # Probe every 10s after that
-                    socket.TCP_KEEPINTVL: env("WS_REDIS_KEEPALIVE_INTERVAL", 10),
+                    socket.TCP_KEEPINTVL: int(env("WS_REDIS_KEEPALIVE_INTERVAL", 10)),
                     # Kill connection after 3 failed probes
-                    socket.TCP_KEEPCNT: env("WS_REDIS_KEEPALIVE_COUNT", 3),
+                    socket.TCP_KEEPCNT: int(env("WS_REDIS_KEEPALIVE_COUNT", 3)),
                 },
             },
         )
