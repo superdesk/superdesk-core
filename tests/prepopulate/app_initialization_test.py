35f6f15100d1add8814fbe308eb3c35bf75f3d66
Brian Mwangi <mwangikabiru21@gmail.com>
2026-03-20 21:19:04 +0300

    [SDBELGA-1059] - Support deletes during init for items marked for deletion (#3178)
    * Support tombstone deletes during init
    
    * Fix tombstone deletes in init data

--- tests/prepopulate/app_initialization_test.py
+++ tests/prepopulate/app_initialization_test.py
@@ -101,6 +101,25 @@ class AppInitializeWithDataCommandTestCase(TestCase):
         self.assertEqual("Urgency", urgency["display_name"])
         self.assertEqual("init", urgency["_etag"])
 
+    def test_init_deletes_tombstone_records(self):
+        self._run(["vocabularies"])
+        urgency = self.app.data.find_one("vocabularies", req=None, _id="urgency")
+        self.assertIsNotNone(urgency)
+
+        init_dir = tempfile.mkdtemp("init", "test")
+        self.app.config.update({"INIT_DATA_PATH": init_dir})
+        self.addCleanup(self.app.config.pop, "INIT_DATA_PATH", None)
+        self.addCleanup(shutil.rmtree, init_dir)
+
+        with open(os.path.join(init_dir, "vocabularies.json"), "w") as f:
+            f.write(json.dumps([{"_id": "urgency", "_deleted": True}]))
+            f.flush()
+
+        self._run(["vocabularies"])
+
+        urgency = self.app.data.find_one("vocabularies", req=None, _id="urgency")
+        self.assertIsNone(urgency)
+
     def test_init_can_combine_files_from_folders(self):
         init_dir = tempfile.mkdtemp("init", "test")
         self.app.config.update({"INIT_DATA_PATH": init_dir})
