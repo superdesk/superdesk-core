817e7c57268e9f58ddd586e05513f8bc7b98cdd7
Petr Jasek <jasekpetr@gmail.com>
2026-04-30 11:41:17 +0200

    fix kombu json encoding error
    when using celery inspect
    
    SDESK-7919

--- superdesk/celery_app/serializer.py
+++ superdesk/celery_app/serializer.py
@@ -4,6 +4,7 @@ from typing import Any, Callable
 
 from eve.io.mongo import MongoJSONEncoder
 from kombu.serialization import register
+from kombu.utils.json import register_type
 
 from superdesk.core import json
 from superdesk.core.types import WSGIApp
@@ -11,6 +12,9 @@ from superdesk.core.utils import str_to_date
 
 
 CELERY_SERIALIZER_NAME = "context-aware/json"
+
+# Allow kombu's own JSON encoder to handle ObjectId (e.g. in celery inspection commands)
+register_type(ObjectId, "objectid", str, ObjectId)
 
 
 class ContextAwareSerializerFactory:
