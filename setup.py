8c4a5e54a38b0771595b615d86a8fef1532dbecd
Petr Jašek <jasekpetr@gmail.com>
2026-06-04 15:06:04 +0200

    fix failing behave tests (#3228)
    * fix failing behave tests
    
    there is a conflict with latest aiohttp and aioresponses
    
    * update comment
    
    * limit the version in setup
    
    so we test with what we use in production
    
    * remove extra import

--- setup.py
+++ setup.py
@@ -66,6 +66,7 @@ install_requires = [
     # Patch Quart, Asyncio to work with Flask extensions
     # TODO-ASYNC: Remove this with our own flask patch (as quart-flask-patch also patches asyncio)
     "quart-flask-patch>=0.3.0,<0.4",
+    "aiohttp<3.14",  # upload.feature tests are failing due to conflict with aioresponses 0.7.8
 ]
 
 package_data = {
