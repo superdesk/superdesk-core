a190cb76a04038de5697b31c679328ab6679297e
Petr Jasek <jasekpetr@gmail.com>
2026-07-09 12:44:36 +0200

    avoid duplicates in publish queue
    make sure one item version is there only once per destination
    
    SDCP-1208

--- superdesk/publish_async/resources/publish_queue/service.py
+++ superdesk/publish_async/resources/publish_queue/service.py
@@ -1,6 +1,8 @@
-from typing import Any
+import logging
+from typing import Any, Sequence
 
 from quart_babel import gettext
+from pymongo.errors import DuplicateKeyError
 
 from superdesk.core import get_current_app
 from superdesk.core.resources import AsyncResourceService
@@ -11,7 +13,111 @@ from superdesk.publish_async.utils import generate_sequence_number
 from superdesk.notification import push_notification
 
 
+logger = logging.getLogger(__name__)
+
+
 class PublishQueueService(AsyncResourceService[PublishQueueResource]):
+    def _get_destination_identity(self, doc: PublishQueueResource) -> tuple[str, str]:
+        if not doc.destination:
+            return "none", "none"
+
+        destination_id = str(doc.destination._id) if doc.destination._id else None
+        if destination_id:
+            return "id", destination_id
+
+        if doc.destination.delivery_type == "content_api":
+            return "content_api", "content_api"
+
+        return (
+            str(doc.destination.delivery_type or "none"),
+            str(doc.destination.name or "none"),
+        )
+
+    def _get_duplicate_lookup(self, doc: PublishQueueResource) -> dict[str, Any]:
+        lookup: dict[str, Any] = {
+            "item_id": doc.item_id,
+            "item_version": doc.item_version,
+            "subscriber_id": doc.subscriber_id,
+            "publishing_action": doc.publishing_action,
+        }
+
+        if doc.destination and doc.destination._id:
+            lookup["destination._id"] = doc.destination._id
+        elif doc.destination and doc.destination.delivery_type == "content_api":
+            lookup["destination.delivery_type"] = "content_api"
+        elif doc.destination:
+            lookup["destination.delivery_type"] = doc.destination.delivery_type
+            lookup["destination.name"] = doc.destination.name
+
+        return lookup
+
+    async def create(self, docs: Sequence[PublishQueueResource | dict[str, Any]]) -> list[PublishQueueResource]:
+        instances = await self._convert_dicts_to_model(docs)
+        created_instances: list[PublishQueueResource] = []
+        seen_identities: set[tuple[Any, ...]] = set()
+
+        for doc in instances:
+            destination_identity_type, destination_identity_value = self._get_destination_identity(doc)
+            identity = (
+                doc.item_id,
+                doc.item_version,
+                doc.subscriber_id,
+                doc.publishing_action,
+                destination_identity_type,
+                destination_identity_value,
+            )
+
+            if identity in seen_identities:
+                logger.warning(
+                    "Skipping duplicate publish queue item in request batch",
+                    extra=dict(
+                        item_id=doc.item_id,
+                        item_version=doc.item_version,
+                        subscriber_id=str(doc.subscriber_id),
+                        publishing_action=doc.publishing_action,
+                        destination_id=doc.destination._id if doc.destination else None,
+                        destination_name=doc.destination.name if doc.destination else None,
+                    ),
+                )
+                continue
+
+            existing = await self.find_one(**self._get_duplicate_lookup(doc))
+            if existing:
+                logger.warning(
+                    "Skipping duplicate publish queue item already in queue",
+                    extra=dict(
+                        item_id=doc.item_id,
+                        item_version=doc.item_version,
+                        subscriber_id=str(doc.subscriber_id),
+                        publishing_action=doc.publishing_action,
+                        destination_id=doc.destination._id if doc.destination else None,
+                        destination_name=doc.destination.name if doc.destination else None,
+                        existing_queue_id=str(existing.id),
+                    ),
+                )
+                seen_identities.add(identity)
+                continue
+
+            seen_identities.add(identity)
+
+            try:
+                created_instances.extend(await super().create([doc]))
+            except DuplicateKeyError:
+                logger.warning(
+                    "Skipping duplicate publish queue item",
+                    extra=dict(
+                        item_id=doc.item_id,
+                        item_version=doc.item_version,
+                        subscriber_id=str(doc.subscriber_id),
+                        publishing_action=doc.publishing_action,
+                        destination_id=doc.destination._id if doc.destination else None,
+                        destination_name=doc.destination.name if doc.destination else None,
+                    ),
+                )
+                continue
+
+        return created_instances
+
     async def on_create(self, docs: list[PublishQueueResource]) -> None:
         await super().on_create(docs)
         subscriber_service = SubscribersResource.get_service()
