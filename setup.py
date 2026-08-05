22e9707b57034935e940eddb881b7a1ab6fc0e54
Petr Jasek <jasekpetr@gmail.com>
2026-08-05 08:43:43 +0200

    release 3.0.0a10

--- setup.py
+++ setup.py
@@ -89,7 +89,7 @@ package_data = {
 
 setup(
     name="Superdesk-Core",
-    version="3.0.0a9",
+    version="3.0.0a10",
     description="Superdesk Core library",
     long_description=LONG_DESCRIPTION,
     author="petr jasek",
--- superdesk/__init__.py
+++ superdesk/__init__.py
@@ -33,7 +33,7 @@ from .signals import *  # noqa
 from apps.common.models.base_model import BaseModel
 from apps.common.components.base_component import BaseComponent
 
-__version__ = "3.0.0a9"
+__version__ = "3.0.0a10"
 
 API_NAME = "Superdesk API"
 SCHEMA_VERSION = 2
